4a7a7bd9d6d77edf6feb1a72cc2b2e98be9625fb
Matt Young <honeybadgerofdoom@gmail.com>
2021-09-13 20:05:18 -0600

    grid work, button

--- client/src/components/SitePages/Login.js
+++ client/src/components/SitePages/Login.js
@@ -7,7 +7,7 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import {Grid, Paper, Typography} from "@material-ui/core";
+import {Button, Grid, Paper, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,13 +15,16 @@ const useStyles = makeStyles((theme) => ({
         flexWrap: 'wrap',
     },
     shortTextField: {
-        width: '25ch',
+        minWidth: '18vw',
         margin: theme.spacing(1),
     },
     paper: {
         width: "60vw",
         margin: theme.spacing(1),
         padding: theme.spacing(2),
+    },
+    formRow: {
+        width: "100%",
     },
 }));
 
@@ -50,97 +53,112 @@ export default function InputAdornments() {
     };
 
     return (
-        <Grid
-            className={classes.root}
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Paper elevation={3} className={classes.paper}>
+        <div>
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+            >
                 <Grid item>
-                    <Typography align="center">Registration</Typography>
-                </Grid>
-                <Grid item>
-                    <FormControl className={classes.shortTextField} variant="outlined">
-                        <InputLabel htmlFor="first-name">First Name</InputLabel>
-                        <OutlinedInput
-                            id="first-name"
-                            value={values.firstName}
-                            onChange={handleChange('firstName')}
-                        />
-                    </FormControl>
-                    <FormControl className={classes.shortTextField} variant="outlined">
-                        <InputLabel htmlFor="last-name">Last Name</InputLabel>
-                        <OutlinedInput
-                            id="last-name"
-                            value={values.lastName}
-                            onChange={handleChange('lastName')}
-                        />
-                    </FormControl>
-                    <FormControl className={classes.shortTextField} variant="outlined">
-                        <InputLabel htmlFor="user-name">Username</InputLabel>
-                        <OutlinedInput
-                            id="user-name"
-                            value={values.userName}
-                            onChange={handleChange('userName')}
-                        />
-                    </FormControl>
+                    <Paper elevation={3} className={classes.paper}>
+                        <Grid
+                            className={classes.root}
+                            container
+                            direction="column"
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <Grid item>
+                                <Typography variant="h4" align="center">Registration</Typography>
+                                <br/>
+                            </Grid>
+                            <Grid item>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="first-name">First Name</InputLabel>
+                                    <OutlinedInput
+                                        id="first-name"
+                                        value={values.firstName}
+                                        onChange={handleChange('firstName')}
+                                    />
+                                </FormControl>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="last-name">Last Name</InputLabel>
+                                    <OutlinedInput
+                                        id="last-name"
+                                        value={values.lastName}
+                                        onChange={handleChange('lastName')}
+                                    />
+                                </FormControl>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="user-name">Username</InputLabel>
+                                    <OutlinedInput
+                                        id="user-name"
+                                        value={values.userName}
+                                        onChange={handleChange('userName')}
+                                    />
+                                </FormControl>
 
+                            </Grid>
+                            <Grid item>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="email">Email Address</InputLabel>
+                                    <OutlinedInput
+                                        id="email"
+                                        value={values.email}
+                                        onChange={handleChange('email')}
+                                    />
+                                </FormControl>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="password">Password</InputLabel>
+                                    <OutlinedInput
+                                        id="password"
+                                        type={values.showPassword ? 'text' : 'password'}
+                                        value={values.password}
+                                        onChange={handleChange('password')}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+                                    <OutlinedInput
+                                        id="confirmPassword"
+                                        type={values.showPassword ? 'text' : 'password'}
+                                        value={values.password}
+                                        onChange={handleChange('confirmPassword')}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item>
+                                <Button className={classes.shortTextField} variant="outlined" size="large">Submit</Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
                 </Grid>
-                <Grid item>
-                    <FormControl className={classes.shortTextField} variant="outlined">
-                        <InputLabel htmlFor="email">Email Address</InputLabel>
-                        <OutlinedInput
-                            id="email"
-                            value={values.email}
-                            onChange={handleChange('email')}
-                        />
-                    </FormControl>
-                    <FormControl className={classes.shortTextField} variant="outlined">
-                        <InputLabel htmlFor="password">Password</InputLabel>
-                        <OutlinedInput
-                            id="password"
-                            type={values.showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            onChange={handleChange('password')}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                    >
-                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                    </FormControl>
-                    <FormControl className={classes.shortTextField} variant="outlined">
-                        <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
-                        <OutlinedInput
-                            id="confirmPassword"
-                            type={values.showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            onChange={handleChange('confirmPassword')}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                    >
-                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                    </FormControl>
-                </Grid>
-            </Paper>
-        </Grid>
+            </Grid>
+        </div>
     );
 }
