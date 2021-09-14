ee1ecd67651f78a387495bfd5d21f0e8fc42837e
April Kelly <wckelly@rams.colostate.edu>
2021-09-13 18:40:09 -0600

    Added sql table creation script.

---
+++ bin/setupTables.sql
@@ -0,0 +1,40 @@
+-- Recreate tables every time this script is run
+DROP TABLE IF EXISTS users,
+                     userProfile,
+                     profiles;
+
+--
+-- Tables:
+--
+
+CREATE TABLE users (
+    userID INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+
+    --These fields are set as fixed width, with the assumption they will hold a SHA-256 hash
+    password CHAR(64) NOT NULL,
+    salt CHAR(64) NOT NULL,
+
+    PRIMARY KEY (userID) 
+);
+
+CREATE TABLE profiles (
+    profileID INT NOT NULL AUTO_INCREMENT,
+    bio VARCHAR(255),
+    picURL VARCHAR(255),
+    PRIMARY KEY (profileID)
+);
+
+-- 
+-- Relations:
+--
+
+CREATE TABLE userProfile (
+    userID INT NOT NULL,
+    profile INT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users (userID),
+    FOREIGN KEY (profileID) REFERENCES profiles (profileID),
+    UNIQUE (userID, profile)
+);
