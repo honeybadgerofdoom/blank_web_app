fc1f9e84750a20feda624ed725f0f263923ffec3
Matt Young <honeybadgerofdoom@gmail.com>
2021-09-14 16:19:32 -0600

    add LoginForm.js and RegistrationForm.js

---
+++ client/src/components/SitePages/LoginForm.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import {Button, Grid, Paper, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    shortTextField: {
+        minWidth: '18vw',
+        margin: theme.spacing(1),
+    },
+    paper: {
+        width: "60vw",
+        margin: theme.spacing(1),
+        padding: theme.spacing(2),
+    },
+    formRow: {
+        width: "100%",
+    },
+}));
+
+export default function LoginForm() {
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        firstName: '',
+        lastName: '',
+        userName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        showPassword: false,
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Grid item>
+                    <Paper elevation={3} className={classes.paper}>
+                        <Grid
+                            className={classes.root}
+                            container
+                            direction="column"
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <Grid item>
+                                <Typography variant="h4" align="center">Log In</Typography>
+                                <br/>
+                            </Grid>
+                            <Grid item>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="user-name">Username</InputLabel>
+                                    <OutlinedInput
+                                        id="user-name"
+                                        value={values.userName}
+                                        onChange={handleChange('userName')}
+                                    />
+                                </FormControl>
+                                <FormControl className={classes.shortTextField} variant="outlined">
+                                    <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+                                    <OutlinedInput
+                                        id="confirmPassword"
+                                        type={values.showPassword ? 'text' : 'password'}
+                                        value={values.password}
+                                        onChange={handleChange('confirmPassword')}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item>
+                                <Button className={classes.shortTextField} variant="outlined" size="large">Submit</Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
