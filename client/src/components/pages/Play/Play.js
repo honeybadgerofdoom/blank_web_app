2e7e74e1adab954a17e39ee08a2544fed5bbaa5c
Matt Young <honeybadgerofdoom@gmail.com>
2021-10-06 14:32:16 -0600

    There's a board

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -1,6 +1,141 @@
 import React from "react";
-import {Typography} from "@material-ui/core";
+import {
+    Grid,
+    makeStyles,
+    Paper,
+    Table,
+    TableBody,
+    TableContainer,
+    TableRow,
+    Typography
+} from "@material-ui/core";
+import Square from "./Square";
+
+const useStyles = makeStyles({
+    title: {
+        margin: "10px",
+    },
+    board: {
+        width: "80vw",
+    },
+});
 
 export default function Play(props) {
-    return <Typography align="center">Play Page</Typography>
+    const classes = useStyles();
+    const pieces = {
+        white: {
+            king: "\u2654",
+            queen: "\u2655",
+            rook: "\u2656",
+            bishop: "\u2657",
+            knight: "\u2658",
+            pawn: "\u2659"
+        },
+        black: {
+            king: "\u265A",
+            queen: "\u265B",
+            rook: "\u265C",
+            bishop: "\u265D",
+            knight: "\u265E",
+            pawn: "\u265F"
+        },
+        empty: "\u1806"
+    }
+
+    return <>
+        <Typography className={classes.title} align="center">Play Page</Typography>
+        <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <TableContainer component={Paper} className={classes.board}>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <Square piece={pieces.black.rook}/>
+                            <Square piece={pieces.black.knight}/>
+                            <Square piece={pieces.black.bishop}/>
+                            <Square piece={pieces.black.queen}/>
+                            <Square piece={pieces.black.king}/>
+                            <Square piece={pieces.black.bishop}/>
+                            <Square piece={pieces.black.knight}/>
+                            <Square piece={pieces.black.rook}/>
+                        </TableRow>
+                        <TableRow>
+                            <Square piece={pieces.black.pawn}/>
+                            <Square piece={pieces.black.pawn}/>
+                            <Square piece={pieces.black.pawn}/>
+                            <Square piece={pieces.black.pawn}/>
+                            <Square piece={pieces.black.pawn}/>
+                            <Square piece={pieces.black.pawn}/>
+                            <Square piece={pieces.black.pawn}/>
+                            <Square piece={pieces.black.pawn}/>
+                        </TableRow>
+                        <TableRow>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                        </TableRow>
+                        <TableRow>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                        </TableRow>
+                        <TableRow>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                        </TableRow>
+                        <TableRow>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                            <Square piece={pieces.empty}/>
+                        </TableRow>
+                        <TableRow>
+                            <Square piece={pieces.white.pawn}/>
+                            <Square piece={pieces.white.pawn}/>
+                            <Square piece={pieces.white.pawn}/>
+                            <Square piece={pieces.white.pawn}/>
+                            <Square piece={pieces.white.pawn}/>
+                            <Square piece={pieces.white.pawn}/>
+                            <Square piece={pieces.white.pawn}/>
+                            <Square piece={pieces.white.pawn}/>
+                        </TableRow>
+                        <TableRow>
+                            <Square piece={pieces.white.rook}/>
+                            <Square piece={pieces.white.knight}/>
+                            <Square piece={pieces.white.bishop}/>
+                            <Square piece={pieces.white.queen}/>
+                            <Square piece={pieces.white.king}/>
+                            <Square piece={pieces.white.bishop}/>
+                            <Square piece={pieces.white.knight}/>
+                            <Square piece={pieces.white.rook}/>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Grid>
+    </>
 }
---
+++ client/src/components/pages/Play/Square.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import {makeStyles, TableCell} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        borderWidth: 1,
+        borderColor: 'black',
+        borderStyle: 'solid',
+        height: "10vh",
+        width: "10vw"
+    },
+});
+
+export default function Play(props) {
+    const classes = useStyles();
+    const [backgroundColor, setBackgroundColor] = useState('#fff');
+
+    function handleClick() {
+        console.log(props.piece);
+        setBackgroundColor('#111');
+    }
+
+    return (
+        <TableCell align="center" className={classes.root} style={{backgroundColor: {backgroundColor}}} onClick={handleClick}>{props.piece}</TableCell>
+    )
+}
