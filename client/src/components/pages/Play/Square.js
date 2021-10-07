f15037ac748213d4a8d29aed1ae9d9ab72ac8de9
Matt Young <honeybadgerofdoom@gmail.com>
2021-10-07 14:03:14 -0600

    Add square coloring

--- client/src/components/pages/Play/Square.js
+++ client/src/components/pages/Play/Square.js
@@ -14,14 +14,29 @@ const useStyles = makeStyles({
 
 export default function Play(props) {
     const classes = useStyles();
+    const squareColor = getSquareColor();
     // const [legalMoves, setLegalMoves] = useState([]);
+
+    function getSquareColor() {
+        const whiteSquare = "#fff5db";
+        const blackSquare = "#a39d8c";
+        return squareIsBlack() ? blackSquare : whiteSquare;
+    }
+
+    function squareIsBlack() {
+        const letter = props.position.substr(0, 1);
+        const number = props.position.substr(1, 1);
+        const blackOdds = ['a', 'c', 'e', 'g'];
+        const blackEvens = ['b', 'd', 'f', 'h'];
+        return blackOdds.includes(letter) && number % 2 || blackEvens.includes(letter) && !(number % 2);
+    }
 
     function handleClick() {
         /*
         This is essentially the code we want to use on the client to make the API request.
         The request is an object with a type of legal moves and a String position.
         The response should be a String array containing the legal moves, as in A2.
-        
+
         const legalMovesResponse = await(sendAPIRequest({requestType: "legalMoves", position: {position}});
         console.log({legalMovesResponse});
         if(legalMovesResponse) {
@@ -32,9 +47,10 @@ export default function Play(props) {
         }
          */
         console.log(props.position);
+        console.log({squareColor});
     }
 
     return (
-        <TableCell align="center" className={classes.root} onClick={handleClick}>{props.piece}</TableCell>
+        <TableCell style={{background: `${squareColor}`}} align="center" className={classes.root} onClick={handleClick}>{props.piece}</TableCell>
     )
 }
