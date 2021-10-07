7a4517e7bc2399b6b7797171f918471c61cacf00
Matt Young <honeybadgerofdoom@gmail.com>
2021-10-07 11:22:26 -0600

    Add commented-out code for sending legalMoves request. This will be a useful reference for the people working on the server-side chess code.

--- client/src/components/pages/Play/Square.js
+++ client/src/components/pages/Play/Square.js
@@ -14,8 +14,23 @@ const useStyles = makeStyles({
 
 export default function Play(props) {
     const classes = useStyles();
+    // const [legalMoves, setLegalMoves] = useState([]);
 
     function handleClick() {
+        /*
+        This is essentially the code we want to use on the client to make the API request.
+        The request is an object with a type of legal moves and a String position.
+        The response should be a String array containing the legal moves, as in A2.
+        
+        const legalMovesResponse = await(sendAPIRequest({requestType: "legalMoves", position: {position}});
+        console.log({legalMovesResponse});
+        if(legalMovesResponse) {
+            setLegalMoves(legalMovesResponse);
+        }
+        else {
+            console.log("ERROR");
+        }
+         */
         console.log(props.position);
     }
 
