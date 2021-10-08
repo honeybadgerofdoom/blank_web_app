f6c2bb233593260bb49bdc21739d2b8910595416
Matt Young <honeybadgerofdoom@gmail.com>
2021-10-08 12:59:37 -0600

    add endpoint to MicroServer

---
+++ server/src/main/java/com/tco/requests/BoardRequest.java
@@ -0,0 +1,15 @@
+package com.tco.requests;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BoardRequest extends Request {
+    private String[64];
+
+    @Override
+    buildResponse() {
+
+    }
+}
