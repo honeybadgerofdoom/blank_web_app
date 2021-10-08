fc6dac4646019c6f7df7afa43f544e78a2ecc7e9
Matt Young <honeybadgerofdoom@gmail.com>
2021-10-08 15:21:08 -0600

    board state is coming from API!

--- client/src/components/pages/Play/Board.js
+++ client/src/components/pages/Play/Board.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Button, makeStyles} from "@material-ui/core";
+import {Button, makeStyles, Typography} from "@material-ui/core";
 import {mockChessboard, positionMap} from "./MockChessboard";
 import Square from "./Square";
 import CustomColumn from "../../../utils/CustomColumn";
@@ -18,23 +18,18 @@ export default function Board() {
     const classes = useStyles();
     const [clickedSquare, setClickedSquare] = useState("");
     const [highlightedSquares, setHighlightedSquares] = useState([]);
+    const [boardState, setBoardState] = useState([]);
 
     async function sendBoardRequest() {
         const boardResponse = await(sendRequest({requestType: "board"}, 'http://localhost:8000'));
-        console.log({boardResponse});
+        if(boardResponse) {
+            setBoardState(boardResponse.boardString.reverse());
+        }
     }
-
-    /*
-    API NOTES
-    Because the client renders things from the top-left to the bottom-right, we need iterate the board in the
-    server-side chess class in that same direction. The response from a /chess/board request should be a
-    String[] built by iterating the ChessBoard from 'h1' -> 'a8' and adding the toString() for each square. If
-    null, add "".
-     */
 
     function renderBoard() {
         return (
-            mockChessboard.map((piece, index) => {
+            boardState.map((piece, index) => {
                 return <Square clickedSquare={clickedSquare} setClickedSquare={setClickedSquare}
                                highlightedSquares={highlightedSquares} setHighlightedSquare={setHighlightedSquares}
                                key={index} piece={piece} position={positionMap[index]}/>
@@ -42,12 +37,17 @@ export default function Board() {
         )
     }
 
-    return (
-        <CustomColumn>
-            <Button onClick={sendBoardRequest}>Send Board Request</Button>
-            <div className={classes.root}>
-                {renderBoard()}
-            </div>
-        </CustomColumn>
-    )
+    if(boardState.length > 0) {
+        return (
+            <CustomColumn>
+                <div className={classes.root}>
+                    {renderBoard()}
+                </div>
+            </CustomColumn>
+        )
+    }
+
+    else {
+        return <Button onClick={sendBoardRequest}>Send Board Request</Button>
+    }
 }
