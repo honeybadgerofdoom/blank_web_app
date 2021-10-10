c26662520d47ff1fd9a3b973d975c53208a4a9c9
darin3 <darinh@rams.colostate.edu>
2021-10-10 16:09:13 -0600

    Reorder code in Database

--- server/src/main/java/com/tco/database/Database.java
+++ server/src/main/java/com/tco/database/Database.java
@@ -4,6 +4,30 @@ import java.sql.*;
 import java.util.*;
 
 public class Database {
+    public static List<Map<String, String>> queryDB(PreparedStatement statement) throws Exception {
+        try (statement; ResultSet results = statement.executeQuery()) {
+            List<Map<String, String>> ret = new ArrayList<>();
+            ResultSetMetaData rsMetaData = results.getMetaData();
+
+            while (results.next()) {
+                ret.add(new HashMap<>());
+                for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
+                    String columnName = rsMetaData.getColumnLabel(i);
+                    String columnValue = results.getString(columnName);
+
+                    ret.get(ret.size()-1).put(columnName, columnValue);
+                }
+            }
+            return ret;
+        }
+    }
+
+    public static int updateDB(PreparedStatement statement) throws Exception {
+        try (statement) {
+            return statement.executeUpdate();
+        }
+    }
+
     public static void main(String[] args) {
         try {
             List<Map<String, String>> rows = queryDB(QueryBuilder.countUsers());
@@ -38,30 +62,6 @@ public class Database {
             }
         } catch (Exception e) {
             System.err.println("Exception: " + e.getMessage());
-        }
-    }
-
-    public static List<Map<String, String>> queryDB(PreparedStatement statement) throws Exception {
-        try (statement; ResultSet results = statement.executeQuery()) {
-            List<Map<String, String>> ret = new ArrayList<>();
-            ResultSetMetaData rsMetaData = results.getMetaData();
-
-            while (results.next()) {
-                ret.add(new HashMap<>());
-                for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
-                    String columnName = rsMetaData.getColumnLabel(i);
-                    String columnValue = results.getString(columnName);
-
-                    ret.get(ret.size()-1).put(columnName, columnValue);
-                }
-            }
-            return ret;
-        }
-    }
-
-    public static int updateDB(PreparedStatement statement) throws Exception {
-        try (statement) {
-            return statement.executeUpdate();
         }
     }
 }
