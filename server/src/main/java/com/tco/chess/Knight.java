6fdf0919d7c9401eb88dc237ee6cd65018eca256
catninja11 <catninja@rams.colostate.edu>
2021-10-10 18:44:19 -0600

    add moves for knight

--- server/src/main/java/com/tco/chess/Knight.java
+++ server/src/main/java/com/tco/chess/Knight.java
@@ -21,6 +21,27 @@ public class Knight extends ChessPiece{
 	
 	@Override
 	public ArrayList<String> legalMoves(){
-		return new ArrayList<String>();
+		ArrayList<String> legalMoves = new ArrayList<String>();
+		int row = this.row;
+		int col = this.column;
+
+		for (int i = -2; i < 3; i++) {
+			for (int j = -2; j < 3; j++) {
+				if(((Math.abs(i) != Math.abs(j)) && (i != 0 && j != 0))) {
+					if(((row + i + 1) < 8 && (row + i + 1) > 0) && (col + j < 8 && col + j >= 0)) {
+						try {
+							String positionString = "" + (Character.toString(((col + j)) + 'a')) + (i + row + 1);
+							ChessPiece positionPiece = board.getPiece(positionString);
+							if (board.getPiece(positionString) == null || positionPiece.getColor() != this.color) {
+								legalMoves.add(positionString);
+							}
+						} catch (IllegalPositionException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+		return legalMoves;
 	}
 }
