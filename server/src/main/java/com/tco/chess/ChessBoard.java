4b29709652b68be5f8c15bf23b7bdf9faac52130
Matt Young <honeybadgerofdoom@gmail.com>
2021-10-15 11:19:51 -0600

    quick refactor

--- server/src/main/java/com/tco/chess/ChessBoard.java
+++ server/src/main/java/com/tco/chess/ChessBoard.java
@@ -62,6 +62,131 @@ public class ChessBoard {
 		
 	}
 
+	public void move(String fromPosition, String toPosition) throws IllegalMoveException {
+
+		if(winner != null) {
+			throw new IllegalMoveException("The game is already over.");
+		}
+
+		try {
+
+			if(!validatePosition(fromPosition) || !validatePosition(toPosition)) {
+				throw new IllegalMoveException("Invalid Input in move");
+			}
+
+			ChessPiece piece = getPiece(fromPosition);
+
+			if(piece == null) {
+				throw new IllegalMoveException("No piece selected");
+			}
+
+			ArrayList<String> legalMoves = piece.legalMoves();
+
+			if(legalMoves.contains(toPosition)) {
+				if(getPiece(toPosition) != null && getPiece(toPosition).getColor() != piece.getColor()) {
+					handleCapture(getPiece(toPosition));
+				}
+				placePiece(piece, toPosition);
+				if ((piece instanceof Rook || piece instanceof King) && !piece.hasMoved) piece.hasMoved = true;
+				switchTurn();
+				checkIfTheGameIsOver();
+			}
+			else {
+				throw new IllegalMoveException("Illegal Move attempted");
+			}
+
+		} catch (IllegalPositionException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void promotePawn(ChessPiece pawn, String promotion) throws IllegalPositionException, IllegalPromotionException {
+		validatePromotion(pawn, promotion);
+
+		Color color = pawn.getColor();
+		String position = pawn.getPosition();
+		int[] rowCol = boardRowCol(position);
+
+		handleCapture(pawn);
+		board[rowCol[0]][rowCol[1]] = null;
+		ChessPiece promotedPiece = createPromotedPiece(promotion, color);
+		placePiece(promotedPiece, position);
+		handlePromotion(promotedPiece);
+		checkIfTheGameIsOver();
+	}
+
+	public void castle(ChessPiece rook, ChessPiece king) throws IllegalMoveException {
+		if (validateCastle(rook, king)) {
+			int kingRow = king.getColor() == Color.WHITE ? 0 : 7;
+			if (rook.column == 0 && queensideCastleIsPossible(kingRow)) {
+				placePiece(rook, rowColToPosition(rook.row, king.column-1));
+				placePiece(king, rowColToPosition(rook.row, rook.column-1));
+				switchTurn();
+			}
+			else if (rook.column == 7 && kingsideCastleIsPossible(kingRow)) {
+				placePiece(rook, rowColToPosition(rook.row, king.column+1));
+				placePiece(king, rowColToPosition(rook.row, rook.column+1));
+				switchTurn();
+			}
+			else throw new IllegalMoveException("Pieces are in the way of castling.");
+		}
+		else throw new IllegalMoveException("Illegal castle attempt.");
+	}
+
+	public boolean placePiece(ChessPiece piece, String position) {
+		try {
+
+			if(!validatePosition(position)) {
+				return false;
+			}
+
+
+			ChessPiece pieceAtPosition = getPiece(position);
+			int[] rowCol = boardRowCol(position);
+
+			if(pieceAtPosition != null && pieceAtPosition.getColor().equals(piece.getColor())) {
+				return false;
+			}
+
+			boolean onBoard = checkPieceOnBoard(piece);
+
+			if(onBoard) {
+				this.board[piece.row][piece.column] = null;
+			}
+
+			//	this.board[piece.row][piece.column] = null;
+			this.board[rowCol[0]][rowCol[1]] = piece;
+
+			piece.setPosition(position);
+
+			// NOTE: At this point, check if the piece is a pawn, and if it is in a position to be promoted. If so,
+			// call promotePawn(). Will implement this later as we really need client/API connection to implement
+			// this in a meaningful way. Thus, this does not *enforce* pawn promotion, it merely makes it possible.
+
+			// NOTE: The response for a /move request will include a boolean saying whether or not a pawn must
+			// 	     be promoted. At which point a /promote request will be made, which will call promotePawn()
+
+			return true;
+
+		}catch(Exception e) {
+			System.out.println("There was an exception in placePiece");
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public ChessPiece getPiece(String position) throws IllegalPositionException {
+
+		if(!validatePosition(position)) {
+			throw new IllegalPositionException("Invalid Board Position");
+		}
+
+		int[] arr = boardRowCol(position);
+
+		return board[arr[0]][arr[1]]; //0 = row, 1 = col
+
+	}
+
 	private boolean validatePosition(String position) {
 		
 		if(position.length() != 2) {
@@ -74,18 +199,6 @@ public class ChessBoard {
 			return false;
 		}
 		return true;
-	}
-	
-	public ChessPiece getPiece(String position) throws IllegalPositionException {
-		
-		if(!validatePosition(position)) {
-			throw new IllegalPositionException("Invalid Board Position");
-		}
-		
-		int[] arr = boardRowCol(position);
-		
-		return board[arr[0]][arr[1]]; //0 = row, 1 = col
-			
 	}
 	
 	private int[] boardRowCol(String position) throws IllegalPositionException {
@@ -114,63 +227,6 @@ public class ChessBoard {
 			}
 		}
 		return onBoard;
-	}
-	
-	public boolean placePiece(ChessPiece piece, String position) {
-		try {
-		
-			if(!validatePosition(position)) {
-				return false;
-			}
-	
-			
-			ChessPiece pieceAtPosition = getPiece(position);
-			int[] rowCol = boardRowCol(position);
-			
-			if(pieceAtPosition != null && pieceAtPosition.getColor().equals(piece.getColor())) {
-				return false;
-			}
-			
-			boolean onBoard = checkPieceOnBoard(piece);
-			
-			if(onBoard) {
-				this.board[piece.row][piece.column] = null; 
-			}
-		
-		//	this.board[piece.row][piece.column] = null; 
-			this.board[rowCol[0]][rowCol[1]] = piece;
-
-			piece.setPosition(position);
-
-			// NOTE: At this point, check if the piece is a pawn, and if it is in a position to be promoted. If so,
-			// call promotePawn(). Will implement this later as we really need client/API connection to implement
-			// this in a meaningful way. Thus, this does not *enforce* pawn promotion, it merely makes it possible.
-
-			// NOTE: The response for a /move request will include a boolean saying whether or not a pawn must
-			// 	     be promoted. At which point a /promote request will be made, which will call promotePawn()
-	
-			return true; 
-		
-		}catch(Exception e) {
-			System.out.println("There was an exception in placePiece");
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	public void promotePawn(ChessPiece pawn, String promotion) throws IllegalPositionException, IllegalPromotionException {
-		validatePromotion(pawn, promotion);
-
-		Color color = pawn.getColor();
-		String position = pawn.getPosition();
-		int[] rowCol = boardRowCol(position);
-
-		handleCapture(pawn);
-		board[rowCol[0]][rowCol[1]] = null;
-		ChessPiece promotedPiece = createPromotedPiece(promotion, color);
-		placePiece(promotedPiece, position);
-		handlePromotion(promotedPiece);
-		checkIfTheGameIsOver();
 	}
 
 	private void validatePromotion(ChessPiece pawn, String promotion) throws IllegalPositionException, IllegalPromotionException {
@@ -212,66 +268,10 @@ public class ChessBoard {
 		return false;
 	}
 
-	public void move(String fromPosition, String toPosition) throws IllegalMoveException {
-
-		if(winner != null) {
-			throw new IllegalMoveException("The game is already over.");
-		}
-		
-		 try {
-			 
-			if(!validatePosition(fromPosition) || !validatePosition(toPosition)) {
-				throw new IllegalMoveException("Invalid Input in move");
-			}
-			
-			ChessPiece piece = getPiece(fromPosition);
-			
-			if(piece == null) {
-				throw new IllegalMoveException("No piece selected");
-			}
-			
-			ArrayList<String> legalMoves = piece.legalMoves();
-			
-			if(legalMoves.contains(toPosition)) {
-				if(getPiece(toPosition) != null && getPiece(toPosition).getColor() != piece.getColor()) {
-					handleCapture(getPiece(toPosition));
-				}
-				placePiece(piece, toPosition);
-				if ((piece instanceof Rook || piece instanceof King) && !piece.hasMoved) piece.hasMoved = true;
-				switchTurn();
-				checkIfTheGameIsOver();
-			}
-			else {
-				throw new IllegalMoveException("Illegal Move attempted");
-			}
-			
-		} catch (IllegalPositionException e) {
-			e.printStackTrace();
-		}
-	}
-
 	private String rowColToPosition(int row, int column) {
 		char letter = (char) (column + 97);
 		int newRow = row + 1;
 		return letter + "" + newRow;
-	}
-
-	public void castle(ChessPiece rook, ChessPiece king) throws IllegalMoveException {
-		if (validateCastle(rook, king)) {
-			int kingRow = king.getColor() == Color.WHITE ? 0 : 7;
-			if (rook.column == 0 && queensideCastleIsPossible(kingRow)) {
-				placePiece(rook, rowColToPosition(rook.row, king.column-1));
-				placePiece(king, rowColToPosition(rook.row, rook.column-1));
-				switchTurn();
-			}
-			else if (rook.column == 7 && kingsideCastleIsPossible(kingRow)) {
-				placePiece(rook, rowColToPosition(rook.row, king.column+1));
-				placePiece(king, rowColToPosition(rook.row, rook.column+1));
-				switchTurn();
-			}
-			else throw new IllegalMoveException("Pieces are in the way of castling.");
-		}
-		else throw new IllegalMoveException("Illegal castle attempt.");
 	}
 
 	public boolean queensideCastleIsPossible(int kingRow) {
@@ -319,12 +319,12 @@ public class ChessBoard {
 		return incrementBasedOnColor;
 	}
 
-	public int[] getPiecesRemaining() {
-		return piecesRemaining;
-	}
-
 	private void switchTurn() {
 		turn = turn == Color.WHITE ? Color.BLACK : Color.WHITE;
+	}
+
+	public int[] getPiecesRemaining() {
+		return piecesRemaining;
 	}
 
 	public Color getWinner() {
