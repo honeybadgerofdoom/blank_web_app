d0abfa177451b41433202220d88953bc9c5f1a0c
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-15 11:49:27 -0600

    test-black

--- server/src/test/java/com/tco/chess/QueenTest.java
+++ server/src/test/java/com/tco/chess/QueenTest.java
@@ -118,6 +118,38 @@ class QueenTest {
    
 }
 
+@Test
+      void testLegalMoves_0_SurroundedByOppositeCOlor() {
+         try {
+           ChessPiece Queen = new Queen(testBoard, Color.BLACK);
+           ChessPiece Pawn1 = new Pawn(testBoard, Color.WHITE);
+           ChessPiece Pawn2 = new Pawn(testBoard, Color.WHITE);
+           ChessPiece Pawn3 = new Pawn(testBoard, Color.WHITE);
+           ChessPiece Pawn4 = new Pawn(testBoard, Color.WHITE);
+           ChessPiece Pawn5 = new Pawn(testBoard, Color.WHITE);
+           ChessPiece Pawn6 = new Pawn(testBoard, Color.WHITE);
+           ChessPiece Pawn7 = new Pawn(testBoard, Color.WHITE);
+           ChessPiece Pawn8 = new Pawn(testBoard, Color.WHITE);
+           testBoard.placePiece(Queen, "f6");
+           testBoard.placePiece(Pawn1, "f7");
+           testBoard.placePiece(Pawn2, "f5");
+           testBoard.placePiece(Pawn3, "e6");
+           testBoard.placePiece(Pawn4, "g6");
+           testBoard.placePiece(Pawn5, "e5");
+           testBoard.placePiece(Pawn6, "e7");
+           testBoard.placePiece(Pawn7, "g5");
+           testBoard.placePiece(Pawn8, "g7");
+           int expect = 8;
+           int actual = Queen.legalMoves().size();
+           assertEquals(expect, actual);
+           } catch (Exception e) {
+           e.printStackTrace();
+       }
+      
+   }
+}
+
+
  
 
 
