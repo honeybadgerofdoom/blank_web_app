e9da5eadbd5f2a10ab17e3e4e2080fb3822184e7
April Kelly <wckelly@rams.colostate.edu>
2021-10-16 15:22:23 -0600

    Hacked together with previous

--- server/src/main/java/com/tco/database/Database.java
+++ server/src/main/java/com/tco/database/Database.java
@@ -11,9 +11,9 @@ public class Database {
         connect();
     }
 
-    private void connect() {
+    private static void connect() {
         try {
-            this.dbc = DriverManager.getConnection(
+            dbc = DriverManager.getConnection(
                     Credential.getConnectionURL(),
                     Credential.USER,
                     Credential.PASSWORD
@@ -27,13 +27,13 @@ public class Database {
     private static PreparedStatement bindParms(PreparedStatement statement, Object... parameters) throws SQLException {
         for (int i = 0; i < parameters.length; i++) {
             if (parameters[i] instanceof String) {
-                statement.setString(i, (String) parameters[i]);
+                statement.setString(i+1, (String) parameters[i]);
             } else if (parameters[i] instanceof Integer) {
-                statement.setInt(i, (Integer) parameters[i]);
+                statement.setInt(i+1, (Integer) parameters[i]);
             } else if (parameters[i] instanceof Double) {
-                statement.setDouble(i, (Double) parameters[i]);
+                statement.setDouble(i+1, (Double) parameters[i]);
             } else if (parameters[i] instanceof Float) {
-                statement.setFloat(i, (Float) parameters[i]);
+                statement.setFloat(i+1, (Float) parameters[i]);
             }
         }
         return statement;
@@ -44,9 +44,22 @@ public class Database {
         return bindParms(statement, parameters);
     }
 
-    public static ResultSet query(String query, Object... parameters) throws SQLException {
+    public static List<Map<String, String>> query(String query, Object... parameters) throws SQLException {
         PreparedStatement statement = prepare(query, parameters);
-        return statement.executeQuery();
+        ResultSet results = statement.executeQuery();
+        List<Map<String, String>> ret = new ArrayList<>();
+        ResultSetMetaData rsMetaData = results.getMetaData();
+
+        while (results.next()) {
+            ret.add(new HashMap<>());
+            for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
+                String columnName = rsMetaData.getColumnLabel(i);
+                String columnValue = results.getString(columnName);
+
+                ret.get(ret.size()-1).put(columnName, columnValue);
+            }
+        }
+        return ret;
     }
 
 
@@ -76,7 +89,22 @@ public class Database {
 
     public static void main(String[] args) {
         try {
-            List<Map<String, String>> rows = queryDB(QueryBuilder.countUsers());
+            connect();
+            String query ="SELECT gameID, userID, board, color, turn, nickname FROM games NATURAL JOIN userGames NATURAL JOIN users WHERE nickname=?";
+            String username = "april";
+            System.out.printf("\nMatches for username '%s':\n\n", username);
+            List<Map<String, String>> rows = query(query, username);
+            for (int i = 0; i < rows.size(); i++) {
+                Map<String, String> row = rows.get(i);
+                System.out.printf("Match %d (gameID=%s)\n", i+1, row.get("gameID"));
+                System.out.printf("  User #%s: %s - color=%s\n", row.get("userID"), row.get("nickname"), row.get("color"));
+                System.out.printf("  Board: %s\n", row.get("board"));
+                boolean isTurn = row.get("color").equals(row.get("turn"));
+                System.out.printf("  %s's turn? %s\n", username, isTurn);
+
+                System.out.println();
+            }
+            /*List<Map<String, String>> rows = queryDB(QueryBuilder.countUsers());
             System.out.println("Total users: " + rows.get(0).get("count"));
 
             System.out.println("\nUsers:\n");
@@ -105,9 +133,10 @@ public class Database {
             int rowsUpdated = updateDB(QueryBuilder.addUser("aaron", "catninja@rams.colostate.edu"));
             if (rowsUpdated == 1) {
                 System.out.println("User successfully added.");
-            }
+            }*/
         } catch (Exception e) {
             System.err.println("Exception: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
