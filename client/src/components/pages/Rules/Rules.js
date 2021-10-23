05e6556190f8a5ccd07e0717e327f804a02f1fa2
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-23 17:10:44 -0600

    format paragraph

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -2,7 +2,18 @@ import React from "react";
 import {Typography} from "@material-ui/core";
 
 export default function Rules(props) {
-     return <><Typography
-        align="center">Rules Page
-    </Typography><Typography display="block"> ____________________________________________________________________________________________ </Typography></>
+     return( <><><Typography align="center"><Typography variant="h3"> Rules for Elimination Chess </Typography></Typography>
+     <Typography
+          display="block">  <Typography align="center"> ____________________________________________________________________ </Typography></Typography> 
+         <Typography display="block">  </Typography>
+         <Typography display="block">  </Typography>
+         <Typography display="block">  </Typography>
+         <Typography paragraph={true}>  Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent.  This can be done by: </Typography>
+        <Typography display="block"> ∙ Capturing the opponent’s  king, </Typography>
+         <Typography display="block">  </Typography>
+
+
+          </></>
+          
+     );
 }
