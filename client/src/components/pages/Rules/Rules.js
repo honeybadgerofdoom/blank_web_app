08542213ef70921d84c4722db41317d58abc7d3a
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-24 11:09:28 -0600

    add indents

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -2,18 +2,26 @@ import React from "react";
 import {Typography} from "@material-ui/core";
 
 export default function Rules(props) {
-     return( <><><Typography align="center"><Typography variant="h3"> Rules for Elimination Chess </Typography></Typography>
+     var combine = OverAllRules.OverAllRules.call();
+     return combine;
+}
+
+const OverAllRules= {
+     OverAllRules: function (){
+          return( <><><Typography align="center"><Typography variant="h3"> Rules for Elimination Chess </Typography></Typography>
      <Typography
           display="block">  <Typography align="center"> ____________________________________________________________________ </Typography></Typography> 
          <Typography display="block">  </Typography>
          <Typography display="block">  </Typography>
          <Typography display="block">  </Typography>
-         <Typography paragraph={true}>  Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent.  This can be done by: </Typography>
-        <Typography display="block"> ∙ Capturing the opponent’s  king, </Typography>
+          <Typography display="block"> </Typography>
+          <Typography> &emsp; &emsp; &emsp;Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent.  This can be done by:  </Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; &emsp;∙ Capturing the opponent’s  king, </Typography>
          <Typography display="block">  </Typography>
 
 
           </></>
           
-     );
+          );
+     }
 }
