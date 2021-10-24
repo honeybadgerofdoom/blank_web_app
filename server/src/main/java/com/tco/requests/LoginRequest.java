e11604f2f10ec7fc914039fe9a79318cc786e6f4
April Kelly <wckelly@rams.colostate.edu>
2021-10-23 21:38:32 -0600

    Added sha256 hash method

--- server/src/main/java/com/tco/requests/LoginRequest.java
+++ server/src/main/java/com/tco/requests/LoginRequest.java
@@ -3,7 +3,12 @@ package com.tco.requests;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.tco.database.Database;
+
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.security.MessageDigest;
+import java.util.Base64;
 
 public class LoginRequest extends Request {
     private final transient Logger log = LoggerFactory.getLogger(LoginRequest.class);
@@ -22,7 +27,28 @@ public class LoginRequest extends Request {
     }
 
     public boolean login(String username, String password) {
-        return false;
+        try (Database db = new Database()) {
+            String query = "SELECT userID " +
+                    "FROM users " +
+                    "WHERE nickname = ? AND password = ?";
+
+            //db.query(query, username, )
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private String sha256(String s) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(s.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
   /* The following methods exist only for testing purposes and are not used
