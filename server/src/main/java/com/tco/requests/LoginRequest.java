333bbd4eacac02ed208e83ebc47895039b443e8d
April Kelly <wckelly@rams.colostate.edu>
2021-10-23 20:55:54 -0600

    Added login schema

---
+++ server/src/main/java/com/tco/requests/LoginRequest.java
@@ -0,0 +1,35 @@
+package com.tco.requests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+public class LoginRequest extends Request {
+    private String serverName;
+    private final transient Logger log = LoggerFactory.getLogger(LoginRequest.class);
+    private ArrayList<String> features;
+
+    @Override
+    public void buildResponse() {
+        serverName = "Team-ReaKt";
+        features = new ArrayList<>();
+        features.add("config");
+        log.trace("buildResponse -> {}", this);
+    }
+
+  /* The following methods exist only for testing purposes and are not used
+  during normal execution, including the constructor. */
+
+    public LoginRequest() {
+        this.requestType = "config";
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public boolean validFeature(String feature){
+        return features.contains(feature);
+    }
+}
