b4a7d53e8c48b9e7dba10a335f8860f65286c37f
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-24 11:23:58 -0600

    finish general rules and formatting

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -11,15 +11,18 @@ const OverAllRules= {
           return( <><><Typography align="center"><Typography variant="h3"> Rules for Elimination Chess </Typography></Typography>
      <Typography
           display="block">  <Typography align="center"> ____________________________________________________________________ </Typography></Typography> 
-         <Typography display="block">  </Typography>
-         <Typography display="block">  </Typography>
-         <Typography display="block">  </Typography>
-          <Typography display="block"> </Typography>
-          <Typography> &emsp; &emsp; &emsp;Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent.  This can be done by:  </Typography>
-         <Typography display="block"> &emsp; &emsp; &emsp; &emsp;∙ Capturing the opponent’s  king, </Typography>
-         <Typography display="block">  </Typography>
-
-
+         <Typography display="block"> &emsp; </Typography>
+          <Typography> &emsp; &emsp; &emsp; Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent.  
+               This can be done by:  </Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing the opponent’s  king, </Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing the opponent’s queen, </Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing both of the opponent's knights, </Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing both of the opponent’s bishops, </Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing both of the opponent's rooks, </Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing all of the opponent’s pawns. </Typography>
+         <Typography> &emsp; &emsp; &emsp;  The current legal moves of each piece will highlight once a piece is selected.  
+              Below is an explanation of all the rules for each piece. </Typography>
+         <Typography display="block"> &emsp; </Typography>
           </></>
           
           );
