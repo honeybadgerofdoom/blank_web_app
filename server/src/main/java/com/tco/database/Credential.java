1014619e22abe503c18b5b1349f5ca668d2976a8
April Kelly <wckelly@rams.colostate.edu>
2021-10-24 13:40:13 -0600

    Fixed incorrect salt and email/nickname reversal

--- server/src/main/java/com/tco/database/Credential.java
+++ server/src/main/java/com/tco/database/Credential.java
@@ -1,14 +1,15 @@
 package com.tco.database;
 
 public class Credential {
-    final static String USER = "darinh";
-    final static String PASSWORD = "831915869";
+    final static String USER = "root";
+    final static String PASSWORD = "gcagca63";
 
     static String getConnectionURL() {
-        String useTunnel = System.getenv("T02_USE_DATABASE_TUNNEL");
+        /*String useTunnel = System.getenv("T02_USE_DATABASE_TUNNEL");
         if (useTunnel != null && useTunnel.equals("true")) {
             return "jdbc:mariadb://127.0.0.1:56247/cs414_team2";
         }
-        return "jdbc:mariadb://faure.cs.colostate.edu:3306/cs414_team2";
+        return "jdbc:mariadb://faure.cs.colostate.edu:3306/cs414_team2";*/
+        return "jdbc:mariadb://127.0.0.1:3306/chess";
     }
 }
