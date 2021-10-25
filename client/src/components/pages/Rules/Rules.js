a02f8c9d126d850b83c0637a78b7cc851c01de07
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-25 17:26:22 -0600

    add all functions to an array

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -2,13 +2,16 @@ import React from "react";
 import {Typography} from "@material-ui/core";
 
 export default function Rules(props) {
-     var combine = OverAllRules.OverAllRules.call();
-     return combine;
+     var rule = [Rule.OverAllRules.call(), Rule.Invite.call() ] ;
+
+     return rule;
+     
 }
 
-const OverAllRules= {
-     OverAllRules: function (){
-          return( <><><Typography align="center"><Typography variant="h3"> Rules for Elimination Chess </Typography></Typography>
+
+const Rule= {
+     OverAllRules: function (props){
+          return( <><><Typography align="center"><Typography variant="h2"> Rules for Elimination Chess </Typography></Typography>
      <Typography
           display="block">  <Typography align="center"> ____________________________________________________________________ </Typography></Typography> 
          <Typography display="block"> &emsp; </Typography>
@@ -21,10 +24,27 @@ const OverAllRules= {
          <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing both of the opponent's rooks, </Typography>
          <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing all of the opponent’s pawns. </Typography>
          <Typography> &emsp; &emsp; &emsp;  The current legal moves of each piece will highlight once a piece is selected.  
-              Below is an explanation of all the rules for each piece. </Typography>
+              Below is an explanation of all the rules for each piece as well as general rules for  </Typography>
+              <Typography display="block"> &emsp; the game. </Typography>
+         <Typography display="block"> &emsp; &emsp; </Typography>
+          </></>
+          
+          );
+     },
+
+     Invite: function (props){
+          return( 
+          <><>
+          <Typography display="block"> &emsp; </Typography>
+          <Typography align="center"><Typography variant="h3"> Inviting someone to a game </Typography></Typography>
+     <Typography
+          display="block">  <Typography align="center"> ____________________________________________________________________ </Typography></Typography> 
+         <Typography display="block"> &emsp; </Typography>
+          <Typography> &emsp; &emsp; &emsp; Well add once we add invitations as a game option  </Typography>
          <Typography display="block"> &emsp; </Typography>
           </></>
           
           );
      }
+     
 }
