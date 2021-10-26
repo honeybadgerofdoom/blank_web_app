fc3007429ab6ebe838fbe3640f87da5d67e31e75
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-25 23:33:34 -0600

    add king

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Typography} from "@material-ui/core";
 
 export default function Rules(props) {
-     var rule = [Rule.OverAllRules.call(), Rule.Invite.call() ] ;
+     var rule = [Rule.OverAllRules.call(), Rule.King.call() ] ;
 
      return rule;
      
@@ -24,24 +24,31 @@ const Rule= {
          <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing both of the opponent's rooks, </Typography>
          <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing all of the opponent’s pawns. </Typography>
          <Typography> &emsp; &emsp; &emsp;  The current legal moves of each piece will highlight once a piece is selected.  
-              Below is an explanation of all the rules for each piece as well as general rules for  </Typography>
-              <Typography display="block"> &emsp; the game. </Typography>
+              Below is an explanation of all the rules for each piece. </Typography>
+         <Typography display="block"> &emsp; &emsp; </Typography>
          <Typography display="block"> &emsp; &emsp; </Typography>
           </></>
           
           );
      },
 
-     Invite: function (props){
+     King: function (props){
           return( 
           <><>
           <Typography display="block"> &emsp; </Typography>
-          <Typography align="center"><Typography variant="h3"> Inviting someone to a game </Typography></Typography>
+          <Typography align="center"><Typography variant="h3"> King</Typography></Typography>
      <Typography
           display="block">  <Typography align="center"> ____________________________________________________________________ </Typography></Typography> 
          <Typography display="block"> &emsp; </Typography>
-          <Typography> &emsp; &emsp; &emsp; Well add once we add invitations as a game option  </Typography>
+          <Typography> &emsp; &emsp; &emsp;   </Typography>
+          <Typography> &emsp; &emsp; &emsp; The king can move one square in any position whether that be horizontal, vertical, or diagonal unless the square is already occupied by your own piece.  </Typography>
+         <Typography display="block"> &emsp; The king also has a special move, called castling.</Typography>
+         <Typography display="block"> &emsp; To be able to accomplish this move, it requires that the king has never moved the entire game,</Typography>
+         <Typography display="block"> &emsp;  and the player will also need to use one of their rooks that have also never moved.</Typography>
+         <Typography display="block"> &emsp; The king will then move two squares towards the rook, or take the rook's spot. </Typography>
+         <Typography display="block"> &emsp; The rook will then move one square in the opposite direction of the king or in between where the rook and king originally were. </Typography>
          <Typography display="block"> &emsp; </Typography>
+          
           </></>
           
           );
