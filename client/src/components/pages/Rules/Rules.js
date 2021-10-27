7de2f7309e0484931c298f0710b4b2b0cb90a520
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-27 07:31:45 -0600

    format

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -42,25 +42,26 @@ const Rule= {
           <Typography display="block">  <Typography align="center"> ____________________________________________________________________ 
           </Typography></Typography> 
           <Typography> &emsp; &emsp; &emsp;   </Typography>
-          <Typography> &emsp; &emsp; &emsp; The pawn is the only piece that can not move backwards. When a pawn initially moves, it has the  
-          option to either move two squares forward, or capture a  </Typography>
-         <Typography display="block"> &emsp;  piece in a diagonal square.  A pawn cannot jump over a piece whether friendly or opponent’s.  After the initial move, a pawn can only move one square forward, 
-           but </Typography>
-         <Typography display="block"> &emsp;  can still capture an opponent’s piece in a diagonal.</Typography>
+          <Typography> &emsp; &emsp; &emsp; The pawn is the only piece that cannot move backwards. When a pawn initially moves, it has the  
+          option to move either one or two squares forward, or   </Typography>
+         <Typography display="block"> &emsp;  capture a piece in a diagonal square.  A pawn cannot jump over a piece whether friendly or
+          opponent’s.  After the initial move, a pawn can only move one square </Typography>
+         <Typography display="block"> &emsp;  forward, but can still capture an opponent’s piece in a diagonal.</Typography>
+         <Typography display="block"> &emsp; </Typography>
           <Typography> &emsp; &emsp; &emsp; Pawn’s are capable of promotion. If a pawn advances all the way to the opposite end of the board, it is  
           promoted to another piece of the player’s choosing. This  </Typography> 
           <Typography display="block"> &emsp;  includes, king, queen, knight, bishop, or rook. The piece will immediately, 
           before the end of turn, be replaced to the new piece.  There is no limit to which  piece you </Typography>
           <Typography display="block"> &emsp;    can promote to,  even if it’s always the same one. </Typography>
-
+          <Typography display="block"> &emsp; </Typography>
           <Typography> &emsp; &emsp; &emsp; The last special move a pawn can do is called en passant.  It can only be done after the initial move of the 
           opponent’s pawn, when it moves two squares 
           </Typography>
-          <Typography display="block"> &emsp;  vertically and it could have been captured if it had only advanced one square.    One the very next turn,
+          <Typography display="block"> &emsp;  vertically and it could have been captured if it had only advanced one square.    On the very next turn,
           after the initial move, the player can place their pawn diagonal, </Typography>
          <Typography display="block"> &emsp; as if the piece were there, and capture the opponent’s pawn.</Typography>
          <Typography display="block"> &emsp; </Typography>
-         
+         <Typography display="block"> &emsp; </Typography>
           
           </></>
           
@@ -77,8 +78,9 @@ const Rule= {
           <Typography> &emsp; &emsp; &emsp;   </Typography>
           <Typography> &emsp; &emsp; &emsp; The knight is unique in that it has the ability to jump over pieces.  It can move two squares 
           vertically, in either direction, and one square horizontally,</Typography>
-         <Typography display="block"> &emsp;  in any direction. The order of this doesn’t matter. 
-          </Typography>
+         <Typography display="block"> &emsp;  in any direction. The order of this doesn’t matter, However, it must move both vertically and 
+         horizontally if piece is played. </Typography>
+         <Typography display="block"> &emsp; </Typography>
          <Typography display="block"> &emsp; </Typography>
           
           </></>
@@ -99,6 +101,7 @@ const Rule= {
          <Typography display="block"> &emsp; There are two bishops per player.  They start on opposite color squares, so their diagonals that they can 
          occupy will always be different.  </Typography>
          <Typography display="block"> &emsp; </Typography>
+         <Typography display="block"> &emsp; </Typography>
           
           </></>
           
@@ -117,6 +120,7 @@ const Rule= {
           from start position to finish position are unoccupied.  </Typography>
           <Typography display="block"> &emsp; There are two rooks for each player that start on opposite colors of the board. </Typography>
          <Typography display="block"> &emsp; </Typography>
+         <Typography display="block"> &emsp; </Typography>
           
           </></>
           
@@ -131,10 +135,11 @@ const Rule= {
           <Typography display="block">  <Typography align="center"> ____________________________________________________________________ 
           </Typography></Typography> 
           <Typography> &emsp; &emsp; &emsp;   </Typography>
-          <Typography> &emsp; &emsp; &emsp; The queen, a very powerful piece in the game, and moves like a bishop and a rook.  It is allowed to 
+          <Typography> &emsp; &emsp; &emsp; The queen, a very powerful piece in the game, moves like a bishop and a rook.  It is allowed to 
           move any number of unoccupied squares vertically,   </Typography>
-         <Typography display="block"> &emsp; horizontally, and diagonally. Because the queen is very powerful, most pawns are promoted to a
-          queen.  </Typography>
+         <Typography display="block"> &emsp; horizontally, or diagonally. The squares inbetween start and finish position must be unoccupied.  
+         Because the queen is very powerful, most pawns are promoted  </Typography>
+         <Typography display="block"> &emsp;  to a queen. </Typography>
          <Typography display="block"> &emsp; </Typography>
          <Typography display="block"> &emsp; </Typography>
           </></>
@@ -154,12 +159,12 @@ const Rule= {
                or diagonally unless the square is already occupied by your own piece.  </Typography>
          <Typography display="block"> &emsp; The king also has a special move, called castling.  To be able to accomplish this move, 
          it requires that the king has never moved the entire game, and the player </Typography>
-         <Typography display="block"> &emsp;   will also need to use one of their rooks that have also never moved.  The king will 
-         then take the rook's spot.   The rook will  then move in the opposite direction </Typography>
-         <Typography display="block"> &emsp;  of the king, jumping over the king, and take the position next to the king.   Keep in mind 
-         to complete this, there must not  be a piece inbetween the rook and </Typography>
-         <Typography display="block"> &emsp;  the king, whether friendly, or the enemies.  
-         Also, if you want to castle, the king must be moved first. Also, if you want to castle, the king must be moved first.</Typography>
+         <Typography display="block"> &emsp;   will also need to use one of their rooks that has also never moved.  The king will 
+         then take the rook's spot.   The rook will  then move in the opposite direction  of the  </Typography>
+         <Typography display="block"> &emsp; king, jumping over the king, and take the position next to the king.   Keep in mind 
+         to complete this, there must not  be a piece inbetween the rook and the king,</Typography>
+         <Typography display="block"> &emsp;   whether friendly, or the enemies.  
+         Also, if you want to castle, the king must be moved first. </Typography> 
          <Typography display="block"> &emsp;  </Typography>
          <Typography display="block"> &emsp; </Typography>
           
