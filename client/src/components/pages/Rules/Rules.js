9c19ea5ce571a76597975a4f573cf21b39540e4e
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-27 06:53:26 -0600

    add pawn

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -42,10 +42,23 @@ const Rule= {
           <Typography display="block">  <Typography align="center"> ____________________________________________________________________ 
           </Typography></Typography> 
           <Typography> &emsp; &emsp; &emsp;   </Typography>
-          <Typography> &emsp; &emsp; &emsp; The knight is unique in that it has the ability to jump over pieces.  It can move two squares 
-          vertically, in either direction, and one square horizontally,</Typography>
-         <Typography display="block"> &emsp;  in any direction. The order of this doesn’t matter. 
+          <Typography> &emsp; &emsp; &emsp; The pawn is the only piece that can not move backwards. When a pawn initially moves, it has the  
+          option to either move two squares forward, or capture a piece in a diagonal square.  A pawn cannot jump over a  </Typography>
+         <Typography display="block"> &emsp;  piece whether friendly or opponent’s.  After the initial move, a pawn can only move one square forward, 
+         but can still capture an opponent’s piece in a diagonal.</Typography>
+
+          <Typography> &emsp; &emsp; &emsp; Pawn’s are capable of promotion. If a pawn advances all the way to the opposite end of the board, it is  
+          promoted to another piece of the player’s choosing. This includes, king, queen, knight, bishop, or rook. The piece will immediately, </Typography> 
+
+          <Typography display="block"> &emsp;  before the end of turn, be replaced to the new piece.  There is no limit to which piece you can promote to, 
+          even if it’s always the same one. </Typography>
+
+          <Typography> &emsp; &emsp; &emsp; The last special move a pawn can do is called en passant.  It can only be done after the initial move of the 
+          opponent’s pawn, when it moves two squares vertically and it could have been captured if it had only advanced one square.    One the very next turn,
+          after the initial move, the player can place their pawn diagonal, as if the piece were there, and capture the opponent’s pawn.
+
           </Typography>
+          <Typography display="block"> &emsp;  </Typography>
          <Typography display="block"> &emsp; </Typography>
           
           </></>
@@ -122,7 +135,7 @@ const Rule= {
          <Typography display="block"> &emsp; horizontally, and diagonally. Because the queen is very powerful, most pawns are promoted to a
           queen.  </Typography>
          <Typography display="block"> &emsp; </Typography>
-          
+         <Typography display="block"> &emsp; </Typography>
           </></>
           
           );
@@ -141,12 +154,10 @@ const Rule= {
          <Typography display="block"> &emsp; The king also has a special move, called castling.  To be able to accomplish this move, 
          it requires that the king has never moved the entire game, and the player </Typography>
          <Typography display="block"> &emsp;   will also need to use one of their rooks that have also never moved.  The king will 
-         then move two squares towards the rook, or take the rook's spot.   The rook will </Typography>
-         <Typography display="block"> &emsp;  then move one square in the opposite direction of the king or in between where the rook 
-         and king originally were.  Keep in mind to complete this, there must not  </Typography>
-         <Typography display="block"> &emsp; be a piece inbetween the rook and the king, whether friendly, or the enemies.  Additional 
-         rules to castling are, the king must not be currently in check and the  </Typography>
-         <Typography display="block"> &emsp; move does not put the king in check.</Typography>
+         then take the rook's spot.   The rook will </Typography>
+         <Typography display="block"> &emsp;  then move in the opposite direction of the king, jumping over the king, and take the position next to the king.   Keep in mind to complete this, there must not  </Typography>
+         <Typography display="block"> &emsp; be a piece inbetween the rook and the king, whether friendly, or the enemies.  </Typography>
+         <Typography display="block"> &emsp;  Also, if you want to castle, the king must be moved first.</Typography>
          <Typography display="block"> &emsp; </Typography>
           
           </></>
