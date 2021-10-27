92ce6481ffef8a72f4f49b72210da5fa5da72c15
bonnie-123 <lani09br@rams.colostate.edu>
2021-10-27 07:18:47 -0600

    format

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -24,7 +24,7 @@ const Rule= {
          <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing both of the opponent’s bishops, </Typography>
          <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing both of the opponent's rooks, </Typography>
          <Typography display="block"> &emsp; &emsp; &emsp; &emsp; &emsp;∙ Capturing all of the opponent’s pawns. </Typography>
-         <Typography display="block"> &emsp; &emsp; All player’s pieces can capture an opponent's piece by taking its position on the board.</Typography>
+         <Typography display="block"> &emsp; &emsp; &emsp; All player’s pieces can capture an opponent's piece by taking its position on the board.</Typography>
          <Typography> &emsp; &emsp; &emsp;  The current legal moves of each piece will highlight once a piece is selected.  
               Below is an explanation of all the rules for each piece. </Typography>
          <Typography display="block"> &emsp; &emsp; </Typography>
@@ -43,23 +43,24 @@ const Rule= {
           </Typography></Typography> 
           <Typography> &emsp; &emsp; &emsp;   </Typography>
           <Typography> &emsp; &emsp; &emsp; The pawn is the only piece that can not move backwards. When a pawn initially moves, it has the  
-          option to either move two squares forward, or capture a piece in a diagonal square.  A pawn cannot jump over a  </Typography>
-         <Typography display="block"> &emsp;  piece whether friendly or opponent’s.  After the initial move, a pawn can only move one square forward, 
-         but can still capture an opponent’s piece in a diagonal.</Typography>
-
+          option to either move two squares forward, or capture a  </Typography>
+         <Typography display="block"> &emsp;  piece in a diagonal square.  A pawn cannot jump over a piece whether friendly or opponent’s.  After the initial move, a pawn can only move one square forward, 
+           but </Typography>
+         <Typography display="block"> &emsp;  can still capture an opponent’s piece in a diagonal.</Typography>
           <Typography> &emsp; &emsp; &emsp; Pawn’s are capable of promotion. If a pawn advances all the way to the opposite end of the board, it is  
-          promoted to another piece of the player’s choosing. This includes, king, queen, knight, bishop, or rook. The piece will immediately, </Typography> 
-
-          <Typography display="block"> &emsp;  before the end of turn, be replaced to the new piece.  There is no limit to which piece you can promote to, 
-          even if it’s always the same one. </Typography>
+          promoted to another piece of the player’s choosing. This  </Typography> 
+          <Typography display="block"> &emsp;  includes, king, queen, knight, bishop, or rook. The piece will immediately, 
+          before the end of turn, be replaced to the new piece.  There is no limit to which  piece you </Typography>
+          <Typography display="block"> &emsp;    can promote to,  even if it’s always the same one. </Typography>
 
           <Typography> &emsp; &emsp; &emsp; The last special move a pawn can do is called en passant.  It can only be done after the initial move of the 
-          opponent’s pawn, when it moves two squares vertically and it could have been captured if it had only advanced one square.    One the very next turn,
-          after the initial move, the player can place their pawn diagonal, as if the piece were there, and capture the opponent’s pawn.
-
+          opponent’s pawn, when it moves two squares 
           </Typography>
-          <Typography display="block"> &emsp;  </Typography>
+          <Typography display="block"> &emsp;  vertically and it could have been captured if it had only advanced one square.    One the very next turn,
+          after the initial move, the player can place their pawn diagonal, </Typography>
+         <Typography display="block"> &emsp; as if the piece were there, and capture the opponent’s pawn.</Typography>
          <Typography display="block"> &emsp; </Typography>
+         
           
           </></>
           
@@ -154,10 +155,12 @@ const Rule= {
          <Typography display="block"> &emsp; The king also has a special move, called castling.  To be able to accomplish this move, 
          it requires that the king has never moved the entire game, and the player </Typography>
          <Typography display="block"> &emsp;   will also need to use one of their rooks that have also never moved.  The king will 
-         then take the rook's spot.   The rook will </Typography>
-         <Typography display="block"> &emsp;  then move in the opposite direction of the king, jumping over the king, and take the position next to the king.   Keep in mind to complete this, there must not  </Typography>
-         <Typography display="block"> &emsp; be a piece inbetween the rook and the king, whether friendly, or the enemies.  </Typography>
-         <Typography display="block"> &emsp;  Also, if you want to castle, the king must be moved first.</Typography>
+         then take the rook's spot.   The rook will  then move in the opposite direction </Typography>
+         <Typography display="block"> &emsp;  of the king, jumping over the king, and take the position next to the king.   Keep in mind 
+         to complete this, there must not  be a piece inbetween the rook and </Typography>
+         <Typography display="block"> &emsp;  the king, whether friendly, or the enemies.  
+         Also, if you want to castle, the king must be moved first. Also, if you want to castle, the king must be moved first.</Typography>
+         <Typography display="block"> &emsp;  </Typography>
          <Typography display="block"> &emsp; </Typography>
           
           </></>
