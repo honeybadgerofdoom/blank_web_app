c881ea3360f63dec2a73b3f021864bfca0c7aca2
kmbear3 <kmbarram@gmail.com>
2021-10-27 16:05:50 -0600

    its broken, but not for long

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Board from "./Board"
+import {sendRequest} from "../../../utils/restfulAPI";
 
 export default function Play(props) {
-    return <Board currentUserID={props.currentUserID}/>
+    const [gameID, setGameID] = useState(null);
+    const [allGames, setAllGames] = useState([]);
+
+    async function sendGameRequest() {
+        const gameResponse = await(sendRequest({requestType: "game", userID: props.currentUserID}, 'http://localhost:8000'));
+        if(gameResponse) {
+
+        }
+        else {
+            console.log("game request failed");
+        }
+    }
+    return <Board currentUserID={props.currentUserID} gameID={gameID}/>
+
 }
---
+++ server/src/main/java/com/tco/requests/GameRequest.java
@@ -0,0 +1,51 @@
+package com.tco.requests;
+
+import java.util.HashMap;
+import com.tco.misc.UnauthorizedRequestException;
+import com.tco.database.Database;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BoardRequest extends Request {
+
+    private final transient Logger log = LoggerFactory.getLogger(GameRequest.class);
+    private int userID;
+
+    private ArrayList<Integer> gameIDs;
+    private boolean success;
+
+    @Override
+    public void buildResponse() {
+        String game = getGameIDsFromDB(this.userID);
+        success = true;
+        log.trace("buildResponse -> {}", this);
+    }
+
+    protected static String getGameIDsFromDB(int userID) {
+        String boardQuery = getDBQueryString();
+        try (Database db = new Database()) {
+            List<Map<String, String>> results = db.query(boardQuery, userID, userID);
+            System.out.println(results);
+            String board = results.get("board");
+            return board;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "----------------------------------------------------------------";
+    }
+
+    private static String getDBQueryString() {
+        return "SELECT * FROM games WHERE player1 = ? OR player2 = ?";
+    }
+
+}
