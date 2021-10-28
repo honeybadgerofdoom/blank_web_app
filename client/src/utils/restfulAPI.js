bffc2328972610527e1c3088a332154020125761
bonnie-123 <54560832+bonnie-123@users.noreply.github.com>
2021-10-28 10:31:18 -0600

    Revert "Register on client"

--- client/src/utils/restfulAPI.js
+++ client/src/utils/restfulAPI.js
@@ -2,13 +2,11 @@ import Ajv from 'ajv';
 import * as configSchema from "../../schemas/ConfigResponse";
 import * as boardSchema from "../../schemas/BoardResponse";
 import * as loginSchema from "../../schemas/LoginResponse";
-import * as registerSchema from "../../schemas/RegisterResponse";
 import { LOG } from "./constants";
 
 const SCHEMAS = {
     config: configSchema,
     board: boardSchema,
-    register: registerSchema,
     login: loginSchema
 };
 
