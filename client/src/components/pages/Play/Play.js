1a0fafb28bcd9c92d4435a5a0e849080c4ac355d
Matt Young <honeybadgerofdoom@gmail.com>
2021-10-28 21:56:34 -0600

    enforce turns, notify client if its not their turn and they try to move

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -2,5 +2,5 @@ import React from "react";
 import Board from "./Board"
 
 export default function Play(props) {
-    return <Board currentUserID={props.currentUserID}/>
+    return <Board currentUserID={props.currentUserID} showMessage={props.showMessage} />
 }
