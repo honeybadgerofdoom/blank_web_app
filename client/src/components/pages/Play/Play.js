1bcb6088a892674afafb873ea8e6ee343164ebd3
kmbear3 <kmbarram@gmail.com>
2021-11-05 13:23:18 -0600

    did the little npm i dance

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
 import Board from "./Board"
 import {sendRequest} from "../../../utils/restfulAPI";
-import {Button} from "@material-ui/core";
+import {Button, List, ListItem, Box} from "@material-ui/core";
+import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemText from '@mui/material/ListItemText';
+// import { FixedSizeList } from 'react-window';
 
 export default function Play(props) {
     const [gameID, setGameID] = useState(null);
@@ -17,6 +20,34 @@ export default function Play(props) {
             console.log("game request failed");
         }
     }
+    // function renderRow(props) {
+    //     const { index, style } = props;
+    //
+    //     return (
+    //         <ListItem style={style} key={index} component="div" disablePadding>
+    //                 <ListItemText primary={`Item ${index + 1}`} />
+    //         </ListItem>
+    //     );
+    // }
+    //
+    //
+    // export default function VirtualizedList() {
+    //     return (
+    //         <Box
+    //             sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
+    //         >
+    //             <FixedSizeList
+    //                 height={400}
+    //                 width={360}
+    //                 itemSize={46}
+    //                 itemCount={200}
+    //                 overscanCount={5}
+    //             >
+    //                 {renderRow}
+    //             </FixedSizeList>
+    //         </Box>
+    //     );
+    // }
     return<>
         <Button variant="outlined" size="large" onClick={sendGameRequest}>Send that game</Button>
         <Board currentUserID={props.currentUserID} showMessage={props.showMessage} chosenGame={allGames[1]} />
