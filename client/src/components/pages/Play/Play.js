fdf620ddda919ce09e46cb6a8c32df1c6e26a020
kmbear3 <kmbarram@gmail.com>
2021-11-05 12:55:16 -0600

    send board request for a specific game

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -6,16 +6,19 @@ import {Button} from "@material-ui/core";
 export default function Play(props) {
     const [gameID, setGameID] = useState(null);
     const [allGames, setAllGames] = useState([]);
+    console.log({allGames})
 
     async function sendGameRequest() {
         const gameResponse = await(sendRequest({requestType: "game", userID: props.currentUserID}));
         if(gameResponse) {
-            console.log({gameResponse});
+            setAllGames(gameResponse.gameIDs);
         }
         else {
             console.log("game request failed");
         }
     }
-    return <Button variant="outlined" size="large" onClick={sendGameRequest}>Send that game</Button>
-    //return <Board currentUserID={props.currentUserID} showMessage={props.showMessage} chosenGameID />
+    return<>
+        <Button variant="outlined" size="large" onClick={sendGameRequest}>Send that game</Button>
+        <Board currentUserID={props.currentUserID} showMessage={props.showMessage} chosenGame={allGames[1]} />
+        </>
 }
