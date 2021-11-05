30a030e1e31bf8a1d9e267e1dbe6c297c43431c7
darin3 <darinh@rams.colostate.edu>
2021-11-04 21:56:05 -0600

    Add support for excludeID field

--- server/src/main/java/com/tco/requests/Users/UsersRequest.java
+++ server/src/main/java/com/tco/requests/Users/UsersRequest.java
@@ -13,15 +13,16 @@ public class UsersRequest extends Request {
     private final static transient int DEFAULT_LIMIT = 50;
 
     private String match;
-    private int limit;
-    private int found;
+    private Integer excludeID;
+    private Integer limit;
+    private Integer found;
     private List<User> users;
 
     @Override
     public void buildResponse() {
         String wildcardMatch = "%" + match + "%";
         try (Database db = new Database()) {
-            this.users = getUsers(db, wildcardMatch, limit);
+            this.users = getUsers(db, wildcardMatch);
             this.found = countUsers(db, wildcardMatch);
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,11 +31,14 @@ public class UsersRequest extends Request {
         log.trace("buildResponse -> {}", this);
     }
 
-    public List<User> getUsers(Database db, String match, int limit) throws SQLException {
-        if (limit == 0)
-            limit = DEFAULT_LIMIT;
+    public List<User> getUsers(Database db, String wildcardMatch) throws SQLException {
+        int actualLimit = (limit > 0) ? limit : DEFAULT_LIMIT;
 
-        List<Map<String, String>> results = db.query(getUsersQuery(), match, limit);
+        Object[] args = (excludeID == null)
+                ? new Object[]{ wildcardMatch, actualLimit }
+                : new Object[]{ wildcardMatch, excludeID, actualLimit };
+
+        List<Map<String, String>> results = db.query(getUsersQuery(), args);
 
         List<User> ret = new ArrayList<>();
         for (Map<String, String> userRow : results) {
@@ -46,8 +50,12 @@ public class UsersRequest extends Request {
         return ret;
     }
 
-    public int countUsers(Database db, String match) throws SQLException {
-        List<Map<String, String>> results = db.query(getCountUsersQuery(), match);
+    public int countUsers(Database db, String wildcardMatch) throws SQLException {
+        Object[] args = (excludeID == null)
+                ? new Object[]{ wildcardMatch }
+                : new Object[]{ wildcardMatch, excludeID };
+
+        List<Map<String, String>> results = db.query(getCountUsersQuery(), args);
         String count = results.get(0).get("count");
         return Integer.parseInt(count);
     }
@@ -56,13 +64,22 @@ public class UsersRequest extends Request {
         return "SELECT userID, nickname, bio " +
                 "FROM users " +
                 "WHERE nickname LIKE ? " +
+                getExcludeIDCheck() +
                 "LIMIT ?";
     }
 
     private String getCountUsersQuery() {
         return "SELECT COUNT(*) AS count " +
                 "FROM users " +
-                "WHERE nickname LIKE ?";
+                "WHERE nickname LIKE ? " +
+                getExcludeIDCheck();
+    }
+
+    private String getExcludeIDCheck() {
+        if (excludeID == null) {
+            return "";
+        }
+        return "AND userID <> ? ";
     }
 
     /* The following methods exist only for testing purposes and are not used
