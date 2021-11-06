203f8e693cb0b36332fb0dd0c76e261f6ae0c57e
bonnie-123 <lani09br@rams.colostate.edu>
2021-11-05 22:22:43 -0600

    map

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -7,78 +7,24 @@ const ruleSetup= {
      " knights, \r∙ Capturing both of the opponent’s bishops, \r∙ Capturing both of the opponent's rooks,\r∙ Capturing all of the "+
      "opponent’s pawns. \rAll player’s pieces can capture an opponent's piece by taking its position on the board.\rThe current legal moves"+
      + " of each piece will highlight once a piece is selected.  \rBelow is an explanation of all the rules for each piece."],
-     headings = ["Pawn", "Knight", "Bishop", "Rook", "Queen", "King"]
-}
-
-
-export default function Rules(props) {
-     var rule = [Rule.OverAllRules.call(), Rule.Pawn.call(), Rule.Knight.call(), 
-          Rule.Bishop.call(), Rule.Rook.call(), Rule.Queen.call(), Rule.King.call() ] ;
-
-     return rule;
      
+     headings = ["Pawn", "Knight", "Bishop", "Rook", "Queen", "King"],
+
+     pieceRules = ["The pawn is the only piece that cannot move backwards. When a pawn initially moves, it has the option to move" +
+     " either one or two squares forward, or capture a piece in a diagonal square.  A pawn cannot jump over a piece whether friendly" +
+      "or opponent’s.  After the initial move, a pawn can only move one square. \r forward, but can still capture an opponent’s piece" +
+      " in a diagonal.Pawn’s are capable of promotion. If a pawn advances all the way to the opposite end of the board, it is promoted" + 
+      " to another piece of the player’s choosing. This includes, king, queen, knight, bishop, or rook. The piece will immediately, " +
+      "before the end of turn, be replaced to the new piece.  There is no limit to which  piece you can promote to,  even if it’s always" +
+      " the same one.  The last special move a pawn can do is called en passant.  It can only be done after the initial move of the opponent’s" +
+      " pawn, when it moves two squares vertically and it could have been captured if it had only advanced one square.    On the very next turn," + 
+      "after the initial move, the player can place their pawn diagonal, as if the piece were there, and capture the opponent’s pawn.", 
+      "The knight is unique in that it has the ability to jump over pieces.  It can move two squares vertically, in either direction, and one" + 
+      " square horizontally, in any direction. The order of this doesn’t matter, However, it must move both vertically and  horizontally if" + 
+      " piece is played."
+     ]
 }
-const Rule= {
-     OverAllRules: function (props){
-          return( <><><Typography align="center"><Typography variant="h2"> Rules for Elimination Chess </Typography></Typography>
-     <Typography
-          display="block">  <Typography align="center"> ____________________________________________________________________ </Typography></Typography> 
-         <Typography display="block"> &emsp; </Typography>
-          </></>
-          );
-     },
 
-
-     Pawn: function (props){
-          return( 
-          <><>
-          <Typography display="block"> &emsp; </Typography>
-          <Typography align="center"><Typography variant="h3"> Pawn </Typography></Typography>
-          <Typography display="block">  <Typography align="center"> ____________________________________________________________________ 
-          </Typography></Typography> 
-          <Typography> &emsp; &emsp; &emsp;   </Typography>
-          <Typography> &emsp; &emsp; &emsp; The pawn is the only piece that cannot move backwards. When a pawn initially moves, it has the  
-          option to move either one or two squares forward, or   </Typography>
-         <Typography display="block"> &emsp;  capture a piece in a diagonal square.  A pawn cannot jump over a piece whether friendly or
-          opponent’s.  After the initial move, a pawn can only move one square </Typography>
-         <Typography display="block"> &emsp;  forward, but can still capture an opponent’s piece in a diagonal.</Typography>
-         <Typography display="block"> &emsp; </Typography>
-          <Typography> &emsp; &emsp; &emsp; Pawn’s are capable of promotion. If a pawn advances all the way to the opposite end of the board, it is  
-          promoted to another piece of the player’s choosing. This  </Typography> 
-          <Typography display="block"> &emsp;  includes, king, queen, knight, bishop, or rook. The piece will immediately, 
-          before the end of turn, be replaced to the new piece.  There is no limit to which  piece you </Typography>
-          <Typography display="block"> &emsp;    can promote to,  even if it’s always the same one. </Typography>
-          <Typography display="block"> &emsp; </Typography>
-          <Typography> &emsp; &emsp; &emsp; The last special move a pawn can do is called en passant.  It can only be done after the initial move of the 
-          opponent’s pawn, when it moves two squares 
-          </Typography>
-          <Typography display="block"> &emsp;  vertically and it could have been captured if it had only advanced one square.    On the very next turn,
-          after the initial move, the player can place their pawn diagonal, </Typography>
-         <Typography display="block"> &emsp; as if the piece were there, and capture the opponent’s pawn.</Typography>
-         <Typography display="block"> &emsp; </Typography>
-         <Typography display="block"> &emsp; </Typography>
-          </></>
-          );
-     },
-
-
-     Knight: function (props){
-          return( 
-          <><>
-          <Typography display="block"> &emsp; </Typography>
-          <Typography align="center"><Typography variant="h3"> Knight </Typography></Typography>
-          <Typography display="block">  <Typography align="center"> ____________________________________________________________________ 
-          </Typography></Typography> 
-          <Typography> &emsp; &emsp; &emsp;   </Typography>
-          <Typography> &emsp; &emsp; &emsp; The knight is unique in that it has the ability to jump over pieces.  It can move two squares 
-          vertically, in either direction, and one square horizontally,</Typography>
-         <Typography display="block"> &emsp;  in any direction. The order of this doesn’t matter, However, it must move both vertically and 
-         horizontally if piece is played. </Typography>
-         <Typography display="block"> &emsp; </Typography>
-         <Typography display="block"> &emsp; </Typography>
-          </></>
-          );
-     },
 
 
      Bishop: function (props){
