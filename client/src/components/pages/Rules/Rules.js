32da5500aad804343e3ef2094fd106ea5b0df822
bonnie-123 <lani09br@rams.colostate.edu>
2021-11-06 16:11:28 -0600

    reformat

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -10,7 +10,7 @@ export default function Rules(props) {
             <Typography align="center" variant="h2">Rules for Elimination Chess</Typography>
             <br/>
             <Grid item>
-            <generalRules.genRules/>
+            <GeneralRules />
             <br/>
             <Grid item>
             <SetupRules/>
@@ -21,25 +21,25 @@ export default function Rules(props) {
      );
 }
 
-const generalRules ={
-    genRules: function (props){
+
+    function GeneralRules(){
           return (
-               <Card key={index} style={{ width: '73rem' }}>
+               <Card style={{ width: '73rem' }}>
                     <CardContent>
                     <div>
-                     <Typography align="left" >&emsp;Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent. This can be done by:  </Typography>
-                     <Typography display="block"> &emsp;&emsp; ∙ Capturing the opponent’s  king,</Typography>
-                     <Typography display="block"> &emsp;&emsp; ∙ Capturing the opponent’s queen,</Typography>
-                     <Typography display="block"> &emsp;&emsp; ∙ Capturing both of the opponent's knights,</Typography>
-                     <Typography display="block"> &emsp;&emsp; ∙ Capturing both of the opponent’s bishops,</Typography>
-                     <Typography display="block"> &emsp;&emsp; ∙ Capturing both of the opponent's rooks,</Typography>
-                     <Typography display="block"> &emsp;&emsp; ∙ Capturing all of the opponent’s pawns.</Typography>
-                     <Typography display="block"> &emsp;All player’s pieces can capture an opponent's piece by taking its position on the board.</Typography>
-                     <Typography display="block"> &emsp;The current legal moves of each piece will highlight once a piece is selected.  Below is an explanation of all the rules for each piece.</Typography>
+                     <Typography align="left" >Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent. This can be done by:  </Typography>
+                     <Typography display="block"> &emsp;∙ Capturing the opponent’s  king,</Typography>
+                     <Typography display="block"> &emsp; ∙ Capturing the opponent’s queen,</Typography>
+                     <Typography display="block"> &emsp; ∙ Capturing both of the opponent's knights,</Typography>
+                     <Typography display="block"> &emsp; ∙ Capturing both of the opponent’s bishops,</Typography>
+                     <Typography display="block"> &emsp; ∙ Capturing both of the opponent's rooks,</Typography>
+                     <Typography display="block"> &emsp; ∙ Capturing all of the opponent’s pawns.</Typography>
+                     <Typography display="block"> All player’s pieces can capture an opponent's piece by taking its position on the board.</Typography>
+                     <Typography display="block"> The current legal moves of each piece will highlight once a piece is selected.  Below is an explanation of all the rules for each piece.</Typography>
                     </div>
                     </CardContent>
-                </Card>           
+               </Card>           
           );
     }
-}
+
 
