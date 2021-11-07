fd4602ef1a34cc6bd360c3f74f82bada3418bd28
Matt Young <honeybadgerofdoom@gmail.com>
2021-11-07 14:36:26 -0700

    table for invites

--- client/src/components/pages/Profile/Profile.js
+++ client/src/components/pages/Profile/Profile.js
@@ -1,7 +1,13 @@
 import React from "react";
 import {Typography} from "@material-ui/core";
+import InvitesTable from "./InvitesTable";
 
 export default function Profile(props) {
     // The current user's username is accessible via props.currentUser
-    return <Typography align="center">Profile Page</Typography>
+    return (
+        <>
+            <Typography align="center">Profile Page</Typography>
+            <InvitesTable userID={props.currentUserID} />
+        </>
+    )
 }
