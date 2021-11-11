d86ba8822d4f57b56faa2eaed681cf07eb89d202
kmbear3 <kmbarram@gmail.com>
2021-11-11 11:49:50 -0700

    move play functions to GameList

---
+++ client/src/components/pages/Play/GameList.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from "react";
+import Board from "./Board"
+import {sendRequest} from "../../../utils/restfulAPI";
+import {Button, List, ListItem, Box, Typography, Paper, makeStyles} from "@material-ui/core";
+import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemText from '@mui/material/ListItemText';
+// import { FixedSizeList } from 'react-window';
+
+const useStyles = makeStyles({
+    root: {
+        overflow: "auto",
+    },
+});
+
+export default function Play(props) {
+    const classes = useStyles();
+    const [allGames, setAllGames] = useState([]);
+
+    useEffect(() => {
+        sendGameRequest(allGames)
+    }, [props.currentUserID]);
+
+    async function sendGameRequest() {
+        const gameResponse = await(sendRequest({requestType: "game", userID: props.currentUserID}));
+        if(gameResponse) {
+            setAllGames(gameResponse.games);
+        }
+        else {
+            console.log("game request failed");
+        }
+    }
+
+    function renderRow() {
+        return allGames.map((game, index)=>{
+            if(game.opponentName !== '') {
+                return (
+                    <ListItem key={index}>
+                        <Board currentUserID={props.currentUserID} showMessage={props.showMessage} chosenGame={game}/>
+                    </ListItem>
+                );
+            }
+            else{
+                return null;
+            }
+        })
+    }
+
+    function displayList() {
+        if(allGames.length !== 0){
+            return (
+                <Paper elevation={3} className={classes.root}>
+                    <List>
+                        {renderRow()}
+                    </List>
+                </Paper>
+            );
+        }
+
+        else {
+            return null
+        }
+    }
+    return<>
+        {displayList()}
+    </>
+}
