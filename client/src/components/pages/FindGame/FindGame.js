0eed5f36b8963764d7ff8556d3b393f30f071d4e
darin3 <darinh@rams.colostate.edu>
2021-11-12 23:41:08 -0700

    Make FindGame return a grid with two tables

--- client/src/components/pages/FindGame/FindGame.js
+++ client/src/components/pages/FindGame/FindGame.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 import InvitesTable from "./InvitesTable";
+import NewGamesTable from "./NewGamesTable";
 
 export default function FindGame(props) {
-    return <InvitesTable userID={props.currentUserID} />
+    return (
+        <Grid container justifyContent="center" >
+            <Grid item>
+                <InvitesTable userID={props.currentUserID} />
+            </Grid>
+            <Grid item>
+                <NewGamesTable userID={props.currentUserID} />
+            </Grid>
+        </Grid>
+    );
 }
