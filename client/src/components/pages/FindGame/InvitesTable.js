fc3743124bbcff6c1a14c5a46d15469b73d8326f
darin3 <darinh@rams.colostate.edu>
2021-11-12 23:43:33 -0700

    Refactor InvitesTable to use helper components

--- client/src/components/pages/FindGame/InvitesTable.js
+++ client/src/components/pages/FindGame/InvitesTable.js
@@ -1,19 +1,11 @@
 import React, {useEffect, useState} from "react";
-import {
-    Button, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableRow, TableHead, TextField, Container
-} from "@material-ui/core";
+import { Button, makeStyles, Paper, TableCell, TableRow, TextField, Container, ButtonGroup } from "@material-ui/core";
+import {TableContent, TableControls} from "./findGameTables";
 import {sendRequest} from "../../../utils/restfulAPI";
 
 const useStyles = makeStyles( {
     root: {
-        margin: "20px",
-    },
-    scrollable: {
-        overflow: "auto",
-        maxHeight: "50vh",
-    },
-    headerText: {
-        fontWeight: "bold",
+        margin: "20px"
     },
     search: {
         width: "100%",
@@ -27,82 +19,65 @@ export default function InvitesTable(props) {
     const [filtering, setFiltering] = useState(false);
 
     const invites = filtering ? filteredInvites : allInvites;
-    console.log({invites})
 
     useEffect(() => {
-        sendMyInvitesRequest();
-    }, [])
-
-    async function sendMyInvitesRequest() {
-        const response = await sendRequest({requestType: "myInvites", userID: props.userID});
-        if(response) {
-            console.log({response})
-            setAllInvites(response.invites);
-            setFilteredInvites(response.invites)
-        }
-        else {
-            console.log("Error with myInvites request")
-        }
-    }
+        sendMyInvitesRequest(props.userID).then((newInvites) => {
+            setAllInvites(newInvites);
+            setFilteredInvites(newInvites)
+        });
+    }, []);
 
     function search(event) {
         const input = event.target.value;
+        const matches = invites.filter(invite => searchForSender(invite, input));
         setFiltering(input !== "");
-        const matches = invites.filter((invite) => searchForSender(invite, input));
         setFilteredInvites(matches);
-    }
-
-    function searchForSender(invite, input) {
-        for(let i = 0; i < input.length; i++) {
-            if(invite.sender.charAt(i) !== input.charAt(i)) return false;
-        }
-        return true;
     }
 
     return (
         <Container maxWidth="sm">
             <Paper elevation={3} className={classes.root}>
-                {TableControls()}
-                <TableContainer component={Paper} className={classes.scrollable}>
-                    <Table>
-                        <TableBody>
-                            {invites.map((invite, index) => {
-                                return (
-                                    <TableRow key={index}>
-                                        <TableCell>{invite.sender}</TableCell>
-                                        <TableCell align="right">{invite.gameID}</TableCell>
-                                        <TableCell align="right"><Button color="primary">Accept</Button></TableCell>
-                                        <TableCell align="right"><Button color="secondary">Decline</Button></TableCell>
-                                    </TableRow>
-                                )
-                            })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                <TableControls title="Join a Match from Invitations">
+                    <TextField className={classes.search} variant="outlined" onChange={search} placeholder="Search my Invites..." />
+                </TableControls>
+                <TableContent headers={["Match ID", "Opponent", "Action"]}>
+                    <MyInviteRows invites={invites} />
+                </TableContent>
             </Paper>
         </Container>
-    )
+    );
+}
 
-    function TableControls() {
-        return (
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell colSpan={5} align="center">
-                                <TextField className={classes.search} variant="outlined" onChange={search} placeholder="Search My Invites..."/>
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell className={classes.headerText}>Opponent</TableCell>
-                            <TableCell className={classes.headerText} align="left">Game ID</TableCell>
-                            <TableCell className={classes.headerText} align="left">Accept</TableCell>
-                            <TableCell className={classes.headerText} align="center">Decline</TableCell>
-                        </TableRow>
-                    </TableHead>
-                </Table>
-            </TableContainer>
-        )
+async function sendMyInvitesRequest(userID) {
+    const response = await sendRequest({ requestType: "myInvites", userID: userID });
+    if (!response) {
+        console.log("Error with myInvites request")
+        return [];
     }
+    //console.log({ myInvitesResponse: response });
+    return response.invites;
+}
 
+function searchForSender(invite, input) {
+    for (let i = 0; i < input.length; i++) {
+        if (invite.sender.charAt(i) !== input.charAt(i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function MyInviteRows(props) {
+    return props.invites.map((invite, index) =>
+        <TableRow key={index}>
+            <TableCell align="center">{invite.gameID}</TableCell>
+            <TableCell align="center">{invite.sender}</TableCell>
+            <TableCell align="right">
+                <ButtonGroup variant="text">
+                    <Button color="primary">Accept</Button>
+                    <Button color="secondary">Decline</Button>
+                </ButtonGroup>
+            </TableCell>
+        </TableRow>
+    );
 }
