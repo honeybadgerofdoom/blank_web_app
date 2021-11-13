5160f7372e0f243ba0a6048d4095c663d3010991
darin3 <darinh@rams.colostate.edu>
2021-11-13 13:56:48 -0700

    Swap invites and new games table

--- client/src/components/pages/FindGame/FindGame.js
+++ client/src/components/pages/FindGame/FindGame.js
@@ -6,11 +6,11 @@ import NewGamesTable from "./NewGamesTable";
 export default function FindGame(props) {
     return (
         <Grid container justifyContent="center" >
-            <Grid item>
-                <InvitesTable userID={props.currentUserID} />
-            </Grid>
-            <Grid item>
+            <Grid item xs={12} md={5}>
                 <NewGamesTable userID={props.currentUserID} />
+            </Grid>
+            <Grid item xs={12} md={5}>
+                <InvitesTable userID={props.currentUserID} />
             </Grid>
         </Grid>
     );
