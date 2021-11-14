c497eea61e8b5fa14a2c87ba75f7df607b082ef9
darin3 <darinh@rams.colostate.edu>
2021-11-13 20:26:53 -0700

    Fix vert scrollbar issue from Grid spacing

--- client/src/components/pages/FindGame/FindGame.js
+++ client/src/components/pages/FindGame/FindGame.js
@@ -1,18 +1,35 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React, {useState} from "react";
+import { Grid, makeStyles } from "@material-ui/core";
 import InvitesTable from "./InvitesTable";
 import NewGamesTable from "./NewGamesTable";
+import UserInvitesModal from "./UserInvitesModal";
+
+const gridSpacing = 6;
+
+const useStyles = makeStyles( {
+    root: {
+        padding: (gridSpacing * 8) / 2  // fix issue with Grid spacing creating scrollbar
+    },
+});
 
 export default function FindGame(props) {
-    const gridItemCols = { xs: 12, sm: 10, md: 5, lg: 4 }
+    const classes = useStyles();
+
+    const [invitesModalInfo, setInvitesModalInfo] = useState({ isOpen: false, gameID: -1 });
+    const openInvitesModal = (gameID=-1) => setInvitesModalInfo({ isOpen: true, gameID: gameID });
+
+    const gridItemCols = { xs: 12, sm: 10, md: 5, lg: 4 };
     return (
-        <Grid className="mt-1" container justifyContent="center" spacing={6}>
-            <Grid item {...gridItemCols}>
-                <NewGamesTable userID={props.currentUserID} />
+        <div className={classes.root}>
+            <Grid container justifyContent="center" spacing={gridSpacing}>
+                <Grid className="mr-1" item {...gridItemCols}>
+                    <NewGamesTable userID={props.currentUserID} openInvitesModal={openInvitesModal} />
+                </Grid>
+                <Grid item {...gridItemCols}>
+                    <InvitesTable userID={props.currentUserID} />
+                </Grid>
+
             </Grid>
-            <Grid item {...gridItemCols}>
-                <InvitesTable userID={props.currentUserID} />
-            </Grid>
-        </Grid>
+        </div>
     );
 }
