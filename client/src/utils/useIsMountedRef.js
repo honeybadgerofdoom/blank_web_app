39a33cd8b39c37432bd8cf3507c7ed05210e9cf8
darin3 <darinh@rams.colostate.edu>
2021-11-15 12:06:31 -0700

    Add useIsMountedRef hook for preventing state updates when not mounted

---
+++ client/src/utils/useIsMountedRef.js
@@ -0,0 +1,10 @@
+import { useEffect, useRef } from "react";
+
+export default function useIsMountedRef() {
+    const isMountedRef = useRef(null);
+    useEffect(() => {
+        isMountedRef.current = true;
+        return () => isMountedRef.current = false;
+    });
+    return isMountedRef;
+}
