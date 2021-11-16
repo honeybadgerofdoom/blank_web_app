ddb83fa42e3ce857dec0aa99fcaceaaa5c516f7b
kmbear3 <kmbarram@gmail.com>
2021-11-16 14:10:00 -0700

    looks better

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -1,16 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import Board from "./Board"
-import {sendRequest} from "../../../utils/restfulAPI";
-import {Button, List, ListItem, Box, Typography, Paper, makeStyles, Grid, Container} from "@material-ui/core";
-import ListItemButton from '@mui/material/ListItemButton';
+import {makeStyles} from "@material-ui/core";
 import GameList from "./GameList";
-import MyUserInfo from "../Profile/MyUserInfo";
-import MyStats from "../Profile/MyStats";
-import ViewOtherUsers from "../Profile/ViewOtherUsers";
 import {Stack} from "@mui/material";
-// import ListItemText from '@mui/material/ListItemText';
-
-// import { FixedSizeList } from 'react-window';
 
 
 const useStyles = makeStyles({
@@ -25,7 +17,7 @@ export default function Play(props) {
     const [chosenGame, setChosenGame] = useState(null);
 
     return(
-        <Stack direction="row" spacing={0} alignItems="flex-start" justifyContent="center" className={classes.root}>
+        <Stack direction="row" spacing={2} alignItems="flex-start" justifyContent="center" className={classes.root}>
             <GameList setChosenGame={setChosenGame} showMessage={props.showMessage} currentUserID={props.currentUserID}/>
             <Board currentUserID={props.currentUserID} showMessage={props.showMessage} chosenGame={chosenGame}/>
         </Stack>
