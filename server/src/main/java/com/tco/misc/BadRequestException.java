4bc2f46ad84c2f5286c54d57175afcfa49068c66
darin3 <darinh@rams.colostate.edu>
2021-11-17 01:35:46 -0700

    Add message constructor to BadRequestException

--- server/src/main/java/com/tco/misc/BadRequestException.java
+++ server/src/main/java/com/tco/misc/BadRequestException.java
@@ -3,4 +3,12 @@ package com.tco.misc;
  * This is a custom exception that fits our personal
  * needs and won't collide with existing issues.
  */
-public class BadRequestException extends Exception {}
+public class BadRequestException extends Exception {
+    public BadRequestException() {
+        super();
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
