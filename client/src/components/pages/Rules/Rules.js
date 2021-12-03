daaf98d053a1a7664297de70795a5e1868eb952c
darin3 <darinh@rams.colostate.edu>
2021-12-02 23:45:52 -0700

    Add spacing to Grid, remove extra items

--- client/src/components/pages/Rules/Rules.js
+++ client/src/components/pages/Rules/Rules.js
@@ -1,45 +1,25 @@
 import React from "react";
-import {Typography, Card, CardContent, CardMedia, Grid, Item} from "@material-ui/core";
-import SetupRules from "./SetupRules";
+import {Typography, Grid, Container, makeStyles} from "@material-ui/core";
+import { GeneralRules, SetupRules } from "./RulesCards";
 
-export default function Rules(props) {
-     return(
-          <Grid container justifyContent="center">
-            <Grid item xs={8}/>
-            <Grid item>
-            <Typography align="center" variant="h2">Rules for Elimination Chess</Typography>
-            <br/>
-            <Grid item>
-            <GeneralRules />
-            <br/>
-            <Grid item>
-            <SetupRules/>
-            </Grid>
-            </Grid>
-            </Grid>
-        </Grid>
+const gridSpacing = 5;
+
+const useStyles = makeStyles({
+    root: {
+        padding: (gridSpacing * 8) / 2  // fix issue with Grid spacing creating scrollbar
+    }
+});
+
+export default function Rules() {
+    const classes = useStyles();
+     return (
+         <Container className={classes.root}>
+             <Typography align="center" variant="h2">Extinction Chess</Typography>
+             <br/>
+             <Grid container justifyContent="center" alignContent="center" spacing={gridSpacing}>
+                 <GeneralRules />
+                 <SetupRules/>
+             </Grid>
+         </Container>
      );
 }
-
-
-    function GeneralRules(){
-          return (
-               <Card style={{ width: '73rem' }}>
-                    <CardContent>
-                    <div>
-                     <Typography align="left" >Extinction Chess is a variant of actual chess.  You win the game by eliminating all of one piece of the opponent. This can be done by:  </Typography>
-                     <Typography display="block"> &emsp;∙ Capturing the opponent’s  king,</Typography>
-                     <Typography display="block"> &emsp; ∙ Capturing the opponent’s queen,</Typography>
-                     <Typography display="block"> &emsp; ∙ Capturing both of the opponent's knights,</Typography>
-                     <Typography display="block"> &emsp; ∙ Capturing both of the opponent’s bishops,</Typography>
-                     <Typography display="block"> &emsp; ∙ Capturing both of the opponent's rooks,</Typography>
-                     <Typography display="block"> &emsp; ∙ Capturing all of the opponent’s pawns.</Typography>
-                     <Typography display="block"> All player’s pieces can capture an opponent's piece by taking its position on the board.</Typography>
-                     <Typography display="block"> The current legal moves of each piece will highlight once a piece is selected.  Below is an explanation of all the rules for each piece.</Typography>
-                    </div>
-                    </CardContent>
-               </Card>           
-          );
-    }
-
-
