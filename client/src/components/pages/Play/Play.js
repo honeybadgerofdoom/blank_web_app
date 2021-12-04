64281a9ae8f625a144b53a3e4d2d6a9eb694f956
darin3 <darinh@rams.colostate.edu>
2021-12-04 13:14:38 -0700

    Move allGames up to Play component so it can be refreshed in QuitGame

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Board from "./Board"
 import {makeStyles} from "@material-ui/core";
 import GameList from "./GameList";
 import {Stack} from "@mui/material";
 import QuitGame from "./QuitGame";
+import {sendRequest} from "../../../utils/restfulAPI";
 
 
 const useStyles = makeStyles({
@@ -15,19 +16,35 @@ const useStyles = makeStyles({
 
 export default function Play(props) {
     const classes = useStyles();
+    const [allGames, setAllGames] = useState([]);
     const [chosenGame, setChosenGame] = useState(null);
     const [highlightedSquares, setHighlightedSquares] = useState([]);
     const [clickedSquare, setClickedSquare] = useState("");
 
+    function refreshGames() {
+        sendGameRequest(props.currentUserID).then(newGames => setAllGames(newGames));
+    }
+
+    useEffect(refreshGames, [props.currentUserID]);
+
     return(
         <Stack direction="row" spacing={2} alignItems="flex-start" justifyContent="center" className={classes.root}>
             <Stack spacing={2} justifyContent="center">
-                <GameList setChosenGame={setChosenGame} showMessage={props.showMessage} currentUserID={props.currentUserID} chosenGame={chosenGame}/>
-                <QuitGame currentUserID={props.currentUserID} chosenGame={chosenGame} setChosenGame={setChosenGame}/>
+                <GameList setChosenGame={setChosenGame} showMessage={props.showMessage} chosenGame={chosenGame} allGames={allGames} />
+                <QuitGame currentUserID={props.currentUserID} chosenGame={chosenGame} setChosenGame={setChosenGame} refreshGames={refreshGames}/>
             </Stack>
             <Board clickedSquare={clickedSquare} setClickedSquare={setClickedSquare} highlightedSquares={highlightedSquares} setHighlightedSquares={setHighlightedSquares} currentUserID={props.currentUserID} showMessage={props.showMessage} chosenGame={chosenGame} setChosenGame={setChosenGame} />
         </Stack>
         
     );
+}
 
+async function sendGameRequest(userID) {
+    const gameResponse = await(sendRequest({requestType: "game", userID: userID, type: "ACTIVE"}));
+    if (gameResponse) {
+        return gameResponse.games;
+    }
+    else {
+        console.log("game request failed");
+    }
 }
