c2c4b417498895ac8a03e3500a5f373149169813
darin3 <darinh@rams.colostate.edu>
2021-12-03 17:56:15 -0700

    Lighter border again

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -12,7 +12,7 @@ const useStyles = makeStyles({
         fontWeight: 600
     },
     card: {
-        border: "3px solid rgba(100, 100, 100, 0.9)",
+        border: "3px solid rgba(180, 180, 180, 1)",
         padding: "10px",
         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
     }
