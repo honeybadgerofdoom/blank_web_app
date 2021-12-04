f46b04252e26c3d0cec8961b2cb4ba57ca3bc571
darin3 <darinh@rams.colostate.edu>
2021-12-04 13:15:37 -0700

    Refresh games after quit request finishes in QuitGame

--- client/src/components/pages/Play/QuitGame.js
+++ client/src/components/pages/Play/QuitGame.js
@@ -1,49 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {sendRequest} from "../../../utils/restfulAPI";
-import {makeStyles} from "@material-ui/core";
 import {Button} from "@mui/material";
 
-const useStyles = makeStyles({
-    root: {
-        overflow: "auto",
-    },
-    container: {
-        maxWidth: "auto"
-    },
-    search: {
-        width: "100%",
-    },
-});
-
 export default function QuitGame(props) {
-    async function sendGameRequest() {
+    async function sendQuitGameRequest() {
         const gameResponse = await(sendRequest({requestType: "quitGame", userID: props.currentUserID, gameID: props.chosenGame.gameID}));
         if (gameResponse) {
-            props.setChosenGame = null;
+            props.setChosenGame(null);
         }
         else {
             console.log("failed to update database after quiting game");
         }
     }
 
-    function quit(){
-        var confirm = window.confirm("Are you sure you would like to quit game?");
+    function quit() {
+        const confirm = window.confirm("Are you sure you would like to quit game?");
         if (confirm) {
-           sendGameRequest();
-           refresh();
+           sendQuitGameRequest().then(() => props.refreshGames());
         }
     }
 
-    function refresh(){
-        window.location.reload(true);
-    }
-
-    if(props.chosenGame) {
+    if (props.chosenGame) {
         return (
             <Button variant="outlined" color="error"  onClick={() =>quit()}> Quit Game </Button>
-    );     
-     }
-    
+        );
+    }
 
     return (props.chosenGame);
 }
