7b919ec803e512df85f338466b20d83ff5e73af1
kmbear3 <kmbarram@gmail.com>
2021-12-04 19:07:40 -0700

    looks good!

--- client/src/components/pages/Play/Play.js
+++ client/src/components/pages/Play/Play.js
@@ -19,15 +19,17 @@ export default function Play(props) {
     const [clickedSquare, setClickedSquare] = useState("");
 
     return(
-        <Stack direction="row" spacing={2} alignItems="flex-start" justifyContent="center" className={classes.root}>
-            <Stack spacing={2} justifyContent="center">
+        <Stack direction="row" spacing={4} alignItems="flex-start" justifyContent="center" className={classes.root}>
+            <Stack spacing={4}>
                 <GameList setChosenGame={setChosenGame} showMessage={props.showMessage} currentUserID={props.currentUserID} chosenGame={chosenGame}/>
                 <QuitGame currentUserID={props.currentUserID} chosenGame={chosenGame} setChosenGame={setChosenGame}/>
             </Stack>
+            <Stack spacing={4} justifyContent="center">
             <Board clickedSquare={clickedSquare} setClickedSquare={setClickedSquare}
                    highlightedSquares={highlightedSquares} setHighlightedSquares={setHighlightedSquares}
                    currentUserID={props.currentUserID} showMessage={props.showMessage} chosenGame={chosenGame}
                    setChosenGame={setChosenGame} flipped={chosenGame && chosenGame.myColor === "BLACK"} />
+            </Stack>
         </Stack>
     );
 }
